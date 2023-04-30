1003bf940145c0a94277b9df8d38ab5c49c93bb9
LightPat <60397186+LightPat@users.noreply.github.com>
2023-04-30 15:44:52 -0400

    character melee UI completed

--- Assets/Plugins/GameCreator/Melee/Mono/Components/UI/CharacterMeleeUI.cs
+++ Assets/Plugins/GameCreator/Melee/Mono/Components/UI/CharacterMeleeUI.cs
@@ -16,21 +16,9 @@
         public TargetCharacter character = new TargetCharacter(TargetCharacter.Target.Player);
         private CharacterMelee melee;
 
-        [Header("Health")]
-        public float currentHP = 100;
         public Slider healthSlider;
-
-        [Header("Poise")]
-        public Image poiseImageFill;
-        public Text poiseTextCurrentValue;
-        public Text poiseTextMaxValue;
-
-        [Header("Defense")]
-        public Image defenseImageFill;
-        public Text defenseTextCurrentValue;
-        public Text defenseTextMaxValue;
-
-        [Header("WeaponUI")]
+        public Slider defenseSlider;
+        public Slider poiseSlider;
         public Image weaponImageFill;
 
         // INITIALIZERS: --------------------------------------------------------------------------
@@ -54,25 +42,9 @@
 
         private void UpdateUI()
         {
-            float healthPercent = currentHP / melee.maxHealth;
-            healthSlider.value = healthPercent;
-
-            float maxPoise = this.melee.maxPoise.GetValue(melee.gameObject);
-            float percentPoise = this.melee.Poise / maxPoise;
-
-            if (this.poiseImageFill) this.poiseImageFill.fillAmount = percentPoise;
-            if (this.poiseTextCurrentValue) this.poiseTextCurrentValue.text = this.melee.Poise.ToString("0.00");
-            if (this.poiseTextMaxValue) this.poiseTextMaxValue.text = maxPoise.ToString("0.00");
-
-            float maxDefense = this.melee.currentShield
-                ? this.melee.currentShield.maxDefense.GetValue(melee.gameObject)
-                : 0f;
-
-            float percentDefense = this.melee.Defense / maxDefense;
-
-            if (this.defenseImageFill) this.defenseImageFill.fillAmount = percentDefense;
-            if (this.defenseTextCurrentValue) this.defenseTextCurrentValue.text = this.melee.Defense.ToString("0.00");
-            if (this.defenseTextMaxValue) this.defenseTextMaxValue.text = maxDefense.ToString("0.00");
+            healthSlider.value = melee.GetHP() / melee.maxHealth;
+            if (melee.currentShield) defenseSlider.value = melee.Defense / melee.currentShield.maxDefense.GetValue(gameObject);
+            poiseSlider.value = melee.Poise / melee.maxPoise.GetValue(gameObject);
         }
 
         /*
