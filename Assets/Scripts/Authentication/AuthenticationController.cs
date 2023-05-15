a6875a8703773e5e47f1471555aa4779cb3522ec
Ej Naranjo <ejvnaranjo@multisyscorp.com>
2023-05-15 23:41:43 +0800

    Fix decrypt

--- Assets/Scripts/Authentication/AuthenticationController.cs
+++ Assets/Scripts/Authentication/AuthenticationController.cs
@@ -10,17 +10,7 @@ public class AuthenticationController : MonoBehaviour
     private const string clientId = "775793118365-5tfdruavpvn7u572dv460i8omc2hmgjt.apps.googleusercontent.com";
     private const string secretId = "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";
     private const string firebaseURL = "https://vithegame-default-rtdb.asia-southeast1.firebasedatabase.app/";
-
-    private void OnEnable()
-    {
-        Application.logMessageReceived += LogMessageReceived;
-    }
-
-    private void LogMessageReceived(string condition, string stacktrace, LogType type)
-    {
-      //  Debug.Log(condition);
-    }
-
+    
     public void SignIn()
     {
         GoogleAuth.Auth(clientId, secretId, (success, error, info) =>
@@ -43,12 +33,10 @@ public class AuthenticationController : MonoBehaviour
 
     private void PostUserdata(UserModel user)
     {
-        RestClient.Post($"{firebaseURL}{AuthHelper.Encrypt(user.email, "vithegame")}.json", user, LoginCallback);
-    }
-
-    private void LoginCallback(RequestException exception, ResponseHelper helper)
-    {
-        Debug.Log(helper.Error);
-        Debug.Log(exception.StatusCode);
+        var _encrypt = AuthHelper.Encrypt(user.email, secretId);
+        RestClient.Put($"{firebaseURL}{Convert.ToBase64String(_encrypt)}/data.json", user);
+        
+        string decryptedString = AuthHelper.Decrypt(Convert.FromBase64String(Convert.ToBase64String(_encrypt)), secretId);
+        Debug.Log(decryptedString);
     }
 }
--- Assets/Scripts/Authentication/Helper/AuthHelper.cs
+++ Assets/Scripts/Authentication/Helper/AuthHelper.cs
@@ -4,14 +4,18 @@ using System.Text;
 
 public class AuthHelper 
 {
-    public static string Encrypt(string plainText, string key)
+    public static byte[] Encrypt(string plainText, string key)
     {
         var iv = new byte[16];
         byte[] array;
 
         using (var aes = Aes.Create())
         {
-            aes.Key = Encoding.UTF8.GetBytes(key);
+            var pdb = new Rfc2898DeriveBytes(key, new byte[] {
+                0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 
+                0x76, 0x65, 0x64, 0x65, 0x76 }, 1000);
+
+            aes.Key = pdb.GetBytes(32);
             aes.IV = iv;
 
             var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
@@ -27,24 +31,36 @@ public class AuthHelper
             }
         }
 
-        return Convert.ToBase64String(array);
+        return array;
     }
+
     
     
-    public static string Decrypt(string encryptedText, string key)
+    public static string Decrypt(byte[] cipherText, string key)
     {
         var iv = new byte[16];
-        var buffer = Convert.FromBase64String(encryptedText);
 
-        using var aes = Aes.Create();
-        aes.Key = Encoding.UTF8.GetBytes(key);
-        aes.IV = iv;
+        using (var aes = Aes.Create())
+        {
+            var pdb = new Rfc2898DeriveBytes(key, new byte[] {
+                0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 
+                0x76, 0x65, 0x64, 0x65, 0x76 }, 1000);
 
-        var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            aes.Key = pdb.GetBytes(32);
+            aes.IV = iv;
 
-        using var ms = new System.IO.MemoryStream(buffer);
-        using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
-        using var sr = new System.IO.StreamReader(cs);
-        return sr.ReadToEnd();
+            var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using (var ms = new System.IO.MemoryStream(cipherText))
+            {
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                {
+                    using (var sr = new System.IO.StreamReader(cs))
+                        return sr.ReadToEnd();
+                }
+            }
+        }
     }
+
+
 }
