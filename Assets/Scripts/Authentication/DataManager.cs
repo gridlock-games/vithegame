445ffdb51d58081e88fe46b8921ab52523aa1e80
Ej Naranjo <ejvnaranjo@multisyscorp.com>
2023-06-07 01:06:34 +0800

    Add firebase store analytics

--- Assets/Scripts/Authentication/DataManager.cs
+++ Assets/Scripts/Authentication/DataManager.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Proyecto26;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DataManager : MonoBehaviour
 {
     public static DataManager Instance { get; private set; }
+    
+    public string clientId = "775793118365-5tfdruavpvn7u572dv460i8omc2hmgjt.apps.googleusercontent.com";
+    public string secretId = "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";
+    public string firebaseURL = "https://vithegame-default-rtdb.asia-southeast1.firebasedatabase.app/";
+    
+    
     [SerializeField] private UserSO UserData;
 
     public UserModel Data {
@@ -31,10 +38,8 @@ public class DataManager : MonoBehaviour
 
     public void LoginSuccess(UserModel _data)
     {
-        Data = _data;
         GameSession(SessionType.LOGIN);
-        //TODO: Load Scene
-        SceneManager.LoadScene("Prototype");
+        SceneManager.LoadScene("Store");
         Debug.Log("Login Success");
     }
     public void GameSession(SessionType type)
@@ -47,5 +52,21 @@ public class DataManager : MonoBehaviour
 
         if (!PlayerPrefs.HasKey("email")) return;
         PlayerPrefs.DeleteKey("email");
+    }
+    
+    public void PostUserdata(UserModel data, bool isLogin = true)
+    {
+        var _encrypt = AuthHelper.Encrypt(data.email, secretId);
+        var _encryptdata = Convert.ToBase64String(_encrypt);
+        if (_encryptdata.Contains('/'))
+        {
+            _encryptdata = _encryptdata.Replace('/', '-');
+        }
+        RestClient.Put($"{firebaseURL}{_encryptdata}/data.json", data, (exception, helper) =>
+        {
+            Data = data;
+            if (!isLogin) return;
+            LoginSuccess(data);
+        });
     }
 }
--- Assets/Scripts/Shops/ShopManager.cs
+++ Assets/Scripts/Shops/ShopManager.cs
@@ -1,5 +1,5 @@
-using System.Security.Cryptography;
-using TMPro;
+using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,6 +15,13 @@ public class ShopManager : MonoBehaviour
     [SerializeField] private Transform spawnLoc;
     
     private GameObject activeCharacter;
+    private DataManager datamanager;
+    private void Start()
+    {
+        datamanager = DataManager.Instance;
+        PostShopAnalytics("Store");
+        
+    }
 
     public void TriggerMainPanel(string _name)
     {
@@ -22,6 +29,9 @@ public class ShopManager : MonoBehaviour
         {
             item.SetActive(item.name == _name);
         }
+
+        if (_name == "Character") return;
+        PostShopAnalytics(_name);
     }
 
     public void DeleteActiveCharacter()
@@ -49,7 +59,74 @@ public class ShopManager : MonoBehaviour
         activeCharacter = Instantiate(_shopModel.characterObject, spawnLoc.position, Quaternion.identity);
         activeCharacter.transform.SetParent(spawnLoc.parent);
         activeCharacter.SetActive(true);
+        PostShopAnalytics("Character", _shopModel.characterName);
     }
 
+    public void PostShopAnalytics(string _panel, string _character = "0")
+    {
+        var _userdata = datamanager.Data;
+        var _shopData = _userdata.shopAnalytics.FirstOrDefault(x => x.panelName == _panel);
 
+        var _panelData = new UserModel.ShopAnalyticsModel
+        {
+            panelName = _panel,
+            dateTime_added = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"),
+            count = 1
+        };
+        if (_shopData == null)
+        {
+            if (_character != "0")
+            {
+                _panelData.character.Add(new UserModel.ShopAnalyticsModel.Characters
+                {
+                    name = _character,
+                    count = 1
+                });
+            }
+            _userdata.shopAnalytics.Add(_panelData);
+            datamanager.PostUserdata(_userdata,false);
+            return;
+        }
+
+        var _index = _userdata.shopAnalytics.IndexOf(_shopData);
+        _panelData.count += _shopData.count;
+        if (_character != "0")
+        {
+            var _chardata  = _userdata.shopAnalytics[_index].character.FirstOrDefault(x => x.name == _character);
+            if (_chardata == null)
+            {
+                _userdata.shopAnalytics[_index].character.Add(new UserModel.ShopAnalyticsModel.Characters
+                {
+                    name = _character,
+                    count = 1
+                });
+            }
+            else
+            {
+                var _charIndex = _userdata.shopAnalytics[_index].character.IndexOf(_chardata);
+                _userdata.shopAnalytics[_index].character[_charIndex] = new UserModel.ShopAnalyticsModel.Characters
+                {
+                    name = _character,
+                    count = _chardata.count + 1
+                };
+            }
+        }
+        else
+        {
+            _userdata.shopAnalytics[_index].character.Add(new UserModel.ShopAnalyticsModel.Characters
+            {
+                name = _character,
+                count = 1
+            });
+        }
+        _userdata.shopAnalytics[_index] = new UserModel.ShopAnalyticsModel
+        {
+            panelName = _panelData.panelName,
+            dateTime_added = _panelData.dateTime_added,
+            count = _panelData.count,
+            character = _userdata.shopAnalytics[_index].character
+        };
+
+        datamanager.PostUserdata(_userdata, false);
+    }
 }
