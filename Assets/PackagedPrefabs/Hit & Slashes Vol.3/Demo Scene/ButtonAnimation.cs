c5ef28892089fca982bba663cd21bdcb1b700c81
Odale Patrick <roxasodale91@gmail.com>
2023-06-16 19:00:43 +0800

    Hits and Slash effects

---
+++ Assets/PackagedPrefabs/Hit & Slashes Vol.3/Demo Scene/ButtonAnimation.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2a35003f84f89eb23602294f046bec1eb82acdb7a5b1df2986232783bfe81d14
+size 920
---
+++ Assets/PackagedPrefabs/Hit & Slashes Vol.3/SelfDestruct.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0809442e660f9110f9da6a70983ee27376cedc023faa8b20ec1a5fc11d3657f
+size 273
