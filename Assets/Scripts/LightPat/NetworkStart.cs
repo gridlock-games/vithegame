5719d4fd5a6965855b5095f6515536b60af1afea
LightPat <60397186+LightPat@users.noreply.github.com>
2023-06-22 18:39:56 -0400

    Register hits by looking up melee clip name

--- Assets/Scripts/LightPat/NetworkStart.cs
+++ Assets/Scripts/LightPat/NetworkStart.cs
@@ -8,8 +8,23 @@ public class NetworkStart : MonoBehaviour
     private void Start()
     {
         if (Application.isEditor)
-            NetworkManager.Singleton.StartHost();
+            NetworkManager.Singleton.StartServer();
         else
             NetworkManager.Singleton.StartClient();
+    }
+
+    private void Update()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            if (!LightPat.Core.ClientManager.Singleton)
+            {
+                foreach (KeyValuePair<ulong, NetworkClient> clientPair in NetworkManager.Singleton.ConnectedClients)
+                {
+                    if (clientPair.Value.PlayerObject)
+                        clientPair.Value.PlayerObject.GetComponent<LightPat.Player.NetworkPlayer>().roundTripTime.Value = NetworkManager.Singleton.GetComponent<NetworkTransport>().GetCurrentRtt(clientPair.Key);
+                }
+            }
+        }
     }
 }
