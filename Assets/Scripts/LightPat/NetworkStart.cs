ba9fd723f1ca0c2ceab1618d4504caed2cc59f46
LightPat <60397186+LightPat@users.noreply.github.com>
2023-07-18 12:41:09 -0400

    Debug FPS on headless builds

--- Assets/Scripts/LightPat/NetworkStart.cs
+++ Assets/Scripts/LightPat/NetworkStart.cs
@@ -2,19 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.Rendering;
 
 public class NetworkStart : MonoBehaviour
 {
     private void Start()
     {
-        if (Application.isEditor)
+        if (SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null)
+            NetworkManager.Singleton.StartServer();
+        else if (Application.isEditor)
             NetworkManager.Singleton.StartHost();
         else
             NetworkManager.Singleton.StartClient();
     }
 
+    private readonly float _hudRefreshRate = 1f;
+    private float _timer;
+
     private void Update()
     {
+        if (SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null)
+        {
+            // FPS Counter and Ping Display
+            if (Time.unscaledTime > _timer)
+            {
+                int fps = (int)(1f / Time.unscaledDeltaTime);
+                Debug.Log(fps + " FPS");
+                _timer = Time.unscaledTime + _hudRefreshRate;
+            }
+        }
+
         if (NetworkManager.Singleton.IsServer)
         {
             if (!LightPat.Core.ClientManager.Singleton)
