f59f1871ee398816260218dfce379febad0706e6
Patrick Seeman <patseem189@gmail.com>
2023-08-14 11:39:50 -0400

    Team Indicator

--- Assets/Plugins/LightPat/Player/TeamIndicator.cs
+++ Assets/Plugins/LightPat/Player/TeamIndicator.cs
@@ -3,36 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using LightPat.Core;
 using Unity.Netcode;
+using System.Linq;
 
 namespace LightPat.Player
 {
-    public class TeamIndicator : MonoBehaviour
+    public class TeamIndicator : NetworkBehaviour
     {
         public GameObject indicatorPrefab;
         public Vector3 indicatorLocalPosition;
         public float glowAmount = 1;
 
-        private Color glowColor = Color.blue;
-
-        private void Start()
-        {
-            GameObject indicatorInstance = Instantiate(indicatorPrefab, transform);
-            indicatorInstance.transform.localPosition = indicatorLocalPosition;
-        }
+        private GameObject indicatorInstance;
 
         private void Update()
         {
             if (ClientManager.Singleton)
             {
-                if (!ClientManager.Singleton.GetClientDataDictionary().ContainsKey(NetworkManager.Singleton.LocalClientId)) { return; }
+                ulong targetClientId = IsSpawned ? OwnerClientId : NetworkManager.Singleton.LocalClientId;
 
+                if (!ClientManager.Singleton.GetClientDataDictionary().ContainsKey(targetClientId)) { return; }
+
+                GameMode[] teamGameModes = new GameMode[] { GameMode.TeamElimination, GameMode.TeamDeathmatch };
+                Team team = ClientManager.Singleton.GetClient(targetClientId).team;
+                if (teamGameModes.Contains(ClientManager.Singleton.gameMode.Value) & (team == Team.Red | team == Team.Blue))
+                {
+                    if (!indicatorInstance)
+                    {
+                        indicatorInstance = Instantiate(indicatorPrefab, transform);
+                        indicatorInstance.transform.localPosition = indicatorLocalPosition;
+                    }
+                }
+                else
+                {
+                    if (indicatorInstance)
+                    {
+                        Destroy(indicatorInstance);
+                    }
+                }
+                
                 foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
                 {
                     foreach (Material mat in renderer.materials)
                     {
                         if (mat.HasProperty("_Glow"))
                         {
-                            Team team = ClientManager.Singleton.GetClient(NetworkManager.Singleton.LocalClientId).team;
                             if (team == Team.Red)
                             {
                                 mat.SetColor("_Color", Color.red);
@@ -45,23 +59,11 @@ namespace LightPat.Player
                             }
                             else
                             {
-                                mat.SetColor("_Color", Color.black);
-                                mat.SetFloat("_Glow", 0);
+                                if (indicatorInstance)
+                                {
+                                    Destroy(indicatorInstance);
+                                }
                             }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
-                {
-                    foreach (Material mat in renderer.materials)
-                    {
-                        if (mat.HasProperty("_Glow"))
-                        {
-                            mat.SetColor("_Color", glowColor);
-                            mat.SetFloat("_Glow", glowAmount);
                         }
                     }
                 }
