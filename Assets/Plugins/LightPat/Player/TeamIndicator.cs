5af7df68461bcee0a03337502efaa6d711d568b6
Patrick Seeman <patseem189@gmail.com>
2023-08-14 11:06:00 -0400

    Ground Circle Team Implementation

--- Assets/Plugins/LightPat/Player/TeamIndicator.cs
+++ Assets/Plugins/LightPat/Player/TeamIndicator.cs
@@ -2,28 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LightPat.Core;
+using Unity.Netcode;
 
 namespace LightPat.Player
 {
     public class TeamIndicator : MonoBehaviour
     {
-        public Color glowColor = Color.red;
-        public float glowAmount = 2.46f;
+        public GameObject indicatorPrefab;
+        public Vector3 indicatorLocalPosition;
+        public float glowAmount = 1;
+
+        private Color glowColor = Color.blue;
 
         private void Start()
         {
-            
+            GameObject indicatorInstance = Instantiate(indicatorPrefab, transform);
+            indicatorInstance.transform.localPosition = indicatorLocalPosition;
         }
 
         private void Update()
         {
             if (ClientManager.Singleton)
             {
+                if (!ClientManager.Singleton.GetClientDataDictionary().ContainsKey(NetworkManager.Singleton.LocalClientId)) { return; }
 
+                foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
+                {
+                    foreach (Material mat in renderer.materials)
+                    {
+                        if (mat.HasProperty("_Glow"))
+                        {
+                            Team team = ClientManager.Singleton.GetClient(NetworkManager.Singleton.LocalClientId).team;
+                            if (team == Team.Red)
+                            {
+                                mat.SetColor("_Color", Color.red);
+                                mat.SetFloat("_Glow", glowAmount);
+                            }
+                            else if (team == Team.Blue)
+                            {
+                                mat.SetColor("_Color", Color.blue);
+                                mat.SetFloat("_Glow", glowAmount);
+                            }
+                            else
+                            {
+                                mat.SetColor("_Color", Color.black);
+                                mat.SetFloat("_Glow", 0);
+                            }
+                        }
+                    }
+                }
             }
             else
             {
-
+                foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
+                {
+                    foreach (Material mat in renderer.materials)
+                    {
+                        if (mat.HasProperty("_Glow"))
+                        {
+                            mat.SetColor("_Color", glowColor);
+                            mat.SetFloat("_Glow", glowAmount);
+                        }
+                    }
+                }
             }
         }
     }
