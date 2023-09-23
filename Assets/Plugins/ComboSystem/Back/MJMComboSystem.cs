fce3eff84830d8bd144d2a5ad06cd245b01c640c
Patrick Seeman <patseem189@gmail.com>
2023-09-22 21:20:05 -0400

    Networked Combo System UI

--- Assets/Plugins/ComboSystem/Back/MJMComboSystem.cs
+++ Assets/Plugins/ComboSystem/Back/MJMComboSystem.cs
@@ -1,37 +1,60 @@
 namespace MJM
 {
-  using GameCreator.Melee;
-  using System;
-  using System.Collections;
-  using System.Collections.Generic;
-  using UnityEngine;
-  using UnityEngine.Events;
+    using GameCreator.Melee;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Events;
+    using Unity.Netcode;
 
-  [Serializable]
-  public class ComboEvent : UnityEvent<int> { }
+    [Serializable]
+    public class ComboEvent : UnityEvent<int> { }
 
-  public class MJMComboSystem : MonoBehaviour
-  {
-    [SerializeField] private CharacterMelee _characterMelee;
-    //UI
-    private int comboCount;
-    public ComboEvent onCounterUpdate;
-
-    public void AddCount(int value)
+    public class MJMComboSystem : NetworkBehaviour
     {
-      comboCount += value;
-      onCounterUpdate.Invoke(comboCount);
-    }
+        [SerializeField] private CharacterMelee _characterMelee;
+        //UI
+        private NetworkVariable<int> comboCount = new NetworkVariable<int>();
+        public ComboEvent onCounterUpdate;
 
-    public void ResetCount()
-    {
-      comboCount = 0;
-      onCounterUpdate.Invoke(comboCount);
-    }
+        public override void OnNetworkSpawn()
+        {
+            comboCount.OnValueChanged += OnComboCountChange;
+        }
 
-    public int GetCount()
-    {
-      return comboCount;
+        public override void OnNetworkDespawn()
+        {
+            comboCount.OnValueChanged -= OnComboCountChange;
+        }
+
+        private void OnComboCountChange(int prev, int current)
+        {
+            onCounterUpdate.Invoke(current);
+
+            if (IsOwner & current == 0)
+            {
+                GetComponentInChildren<ComboUIAnimator>().HideComboUI();
+            }
+        }
+
+        public void AddCount(int value)
+        {
+            if (!IsServer) { return; }
+
+            comboCount.Value += value;
+        }
+
+        public void ResetCount()
+        {
+            if (!IsServer) { return; }
+
+            comboCount.Value = 0;
+        }
+
+        public int GetCount()
+        {
+            return comboCount.Value;
+        }
     }
-  }
 }
