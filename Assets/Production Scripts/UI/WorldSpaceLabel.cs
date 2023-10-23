386590f202f48eb1892264e7b78ab5d228852a12
Patrick Seeman <patseem189@gmail.com>
2023-10-23 13:51:27 -0400

    world space label scaling improvements

--- Assets/Production Scripts/UI/WorldSpaceLabel.cs
+++ Assets/Production Scripts/UI/WorldSpaceLabel.cs
@@ -9,9 +9,10 @@ namespace Vi.UI
     public class WorldSpaceLabel : MonoBehaviour
     {
         [Header("Visual Settings")]
-        [SerializeField] private float rotationSpeed = 1;
-        [SerializeField] private float scalingSpeed = 1;
-        [SerializeField] private float viewDistance = 10;
+        [SerializeField] private float rotationSpeed = 15;
+        [SerializeField] private float scalingSpeed = 8;
+        [SerializeField] private float scalingDistanceDivisor = 500;
+        [SerializeField] private float viewDistance = 20;
 
         [Header("Object Assignments")]
         [SerializeField] private Text nameDisplay;
@@ -44,16 +45,33 @@ namespace Vi.UI
 
         private void LateUpdate()
         {
-            Vector3 pos = rendererToFollow.bounds.center;
-            pos.y += rendererToFollow.bounds.extents.y * 10;
-            transform.position = pos;
-            transform.localScale = Vector3.one * 0.01f;
-
+            Vector3 localScaleTarget = Vector3.zero;
             if (Camera.main)
             {
-                Quaternion targetRotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position);
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Camera.main.transform.position - transform.position), Time.deltaTime * rotationSpeed);
+
+                float camDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
+                if (camDistance > viewDistance)
+                {
+                    Quaternion mouseOverRotation = Quaternion.LookRotation(Camera.main.transform.position - rendererToFollow.bounds.center);
+                    float upAngle = Vector3.SignedAngle(Camera.main.transform.up, mouseOverRotation * Vector3.up, Camera.main.transform.right);
+                    float horizontalAngle = Quaternion.Angle(Camera.main.transform.rotation, mouseOverRotation);
+                    localScaleTarget = horizontalAngle > 178 & (upAngle > -4 & upAngle <= 0) ? Vector3.one * (camDistance / scalingDistanceDivisor) : Vector3.zero;
+                }
+                else
+                {
+                    localScaleTarget = Vector3.one * (camDistance / scalingDistanceDivisor);
+                }
+
+                Vector3 pos = rendererToFollow.bounds.center;
+                pos.y += rendererToFollow.bounds.extents.y * transform.localScale.x * scalingDistanceDivisor;
+                transform.position = pos;
             }
+            else
+            {
+                Debug.LogWarning("Can't find a main camera for world space labels!");
+            }
+            transform.localScale = Vector3.Lerp(transform.localScale, localScaleTarget, Time.deltaTime * scalingSpeed);
 
             // Scale size of name background by size of text
             if (nameBackground.TryGetComponent(out RectTransform rectTransform))
