b3618bb8830ac6893a9967a9eb08c39483885edd
Patrick Seeman <patseem189@gmail.com>
2023-11-04 17:37:50 -0400

    Remove extra references from limb references

--- Assets/Production Scripts/Core/LimbReferences.cs
+++ Assets/Production Scripts/Core/LimbReferences.cs
@@ -58,22 +58,10 @@ namespace Vi.Core
         public RigWeightTarget GetRightHandReachRig() { return rightHandReachRig; }
         public RigWeightTarget GetLeftHandReachRig() { return leftHandReachRig; }
 
-        [SerializeField] private GameObject rightHand;
-        [SerializeField] private GameObject leftHand;
-
         [Header("IK Settings")]
         [SerializeField] private RigWeightTarget rightHandAimRig;
         [SerializeField] private RigWeightTarget leftHandAimRig;
         [SerializeField] private RigWeightTarget rightHandReachRig;
         [SerializeField] private RigWeightTarget leftHandReachRig;
-
-        [Header("Hand IK Settings")]
-        public Vector3 rightHandAimForwardDir = new Vector3(0, 0, 1);
-        public Vector3 rightHandAimUpDir = new Vector3(0, 1, 0);
-
-        public Vector3 rightHandAimIKOffset;
-
-        public Vector3 leftHandPosOffset;
-        public Vector3 leftHandRotOffset;
     }
 }
