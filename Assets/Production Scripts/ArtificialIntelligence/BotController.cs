4753921a13b9315ae367b5baf838f0497c640fcc
Patrick Seeman <patseem189@gmail.com>
2023-11-19 00:59:34 -0500

    new bot controller

--- Assets/Production Scripts/ArtificialIntelligence/BotController.cs
+++ Assets/Production Scripts/ArtificialIntelligence/BotController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vi.Core;
 using Unity.Netcode;
+using Vi.ScriptableObjects;
 
 namespace Vi.ArtificialIntelligence
 {
@@ -27,51 +28,96 @@ namespace Vi.ArtificialIntelligence
             animationHandler = GetComponent<AnimationHandler>();
             weaponHandler = GetComponent<WeaponHandler>();
             attributes = GetComponent<Attributes>();
+            movementPredictionRigidbody.transform.SetParent(null, true);
+            targetMovementPredictionRigidbodyPosition = transform.position;
         }
 
-        private void TryMove(Vector3 movement)
+        public override void ReceiveOnCollisionEnterMessage(Collision collision)
         {
-            RaycastHit[] allHits = Physics.CapsuleCastAll(transform.position + transform.rotation * animationHandler.LimbReferences.bottomPointOfCapsuleOffset,
-                                                          transform.position + transform.up * animationHandler.LimbReferences.characterHeight,
-                                                          animationHandler.LimbReferences.characterRadius, movement, movement.magnitude, Physics.AllLayers, QueryTriggerInteraction.Ignore);
-            System.Array.Sort(allHits, (x, y) => x.distance.CompareTo(y.distance));
+            targetMovementPredictionRigidbodyPosition = movementPredictionRigidbody.position;
+        }
 
-            Vector3 newPosition = transform.position;
+        public override void ReceiveOnCollisionStayMessage(Collision collision)
+        {
+            targetMovementPredictionRigidbodyPosition = movementPredictionRigidbody.position;
+        }
+
+        [SerializeField] private Rigidbody movementPredictionRigidbody;
+        private Vector3 targetMovementPredictionRigidbodyPosition;
+        private float positionStrength = 1;
+        private float runSpeed = 5;
+        private bool isGrounded = true;
+        private float runAnimationTransitionSpeed = 5;
+        private void FixedUpdate()
+        {
+            if (attributes.GetAilment() == ActionClip.Ailment.Death) { movementPredictionRigidbody.velocity = Vector3.zero; return; }
+            if (!CanMove()) { return; }
+
+            Vector3 movement = Vector3.zero;
+            if (moveToPlayer)
+            {
+                if (Vector3.Distance(NetworkManager.LocalClient.PlayerObject.transform.position, transform.position) > 1.5f)
+                {
+                    Vector3 target = new Vector3(NetworkManager.LocalClient.PlayerObject.transform.position.x, transform.position.y, NetworkManager.LocalClient.PlayerObject.transform.position.z);
+                    Vector3 dir = Vector3.ClampMagnitude(target - transform.position, 1);
+                    movement = isGrounded ? runSpeed * Time.fixedDeltaTime * dir : Vector3.zero;
+                    Vector3 animDir = transform.rotation * dir;
+
+                    if (dir == Vector3.zero)
+                        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.identity, Time.deltaTime * 540);
+                    else
+                        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 540);
+
+                    animationHandler.Animator.SetFloat("MoveForward", Mathf.MoveTowards(animationHandler.Animator.GetFloat("MoveForward"), animDir.z, Time.deltaTime * runAnimationTransitionSpeed));
+                    animationHandler.Animator.SetFloat("MoveSides", Mathf.MoveTowards(animationHandler.Animator.GetFloat("MoveSides"), animDir.x, Time.deltaTime * runAnimationTransitionSpeed));
+                }
+                else
+                {
+                    animationHandler.Animator.SetFloat("MoveForward", Mathf.MoveTowards(animationHandler.Animator.GetFloat("MoveForward"), 0, Time.deltaTime * runAnimationTransitionSpeed));
+                    animationHandler.Animator.SetFloat("MoveSides", Mathf.MoveTowards(animationHandler.Animator.GetFloat("MoveSides"), 0, Time.deltaTime * runAnimationTransitionSpeed));
+                }
+            }
+
+            if (animationHandler.ShouldApplyRootMotion())
+            {
+                movement = animationHandler.ApplyLocalRootMotion();
+            }
+
+            if (canLightAttack)
+            {
+                if (Vector3.Distance(NetworkManager.LocalClient.PlayerObject.transform.position, transform.position) < 1.5f)
+                {
+                    SendMessage("OnLightAttack");
+                }
+            }
+
+            // Handle gravity
+            RaycastHit[] allHits = Physics.SphereCastAll(targetMovementPredictionRigidbodyPosition + transform.rotation * animationHandler.LimbReferences.bottomPointOfCapsuleOffset,
+                                            animationHandler.LimbReferences.characterRadius, Physics.gravity, Physics.gravity.magnitude, ~LayerMask.GetMask(new string[] { "NetworkPrediction" }), QueryTriggerInteraction.Ignore);
+            System.Array.Sort(allHits, (x, y) => x.distance.CompareTo(y.distance));
+            Vector3 gravity = Vector3.zero;
             bool bHit = false;
             foreach (RaycastHit hit in allHits)
             {
-                if (hit.distance == 0) { continue; }
                 if (hit.transform.root == transform) { continue; }
+                gravity += Time.fixedDeltaTime * Mathf.Clamp01(hit.distance) * Physics.gravity;
                 bHit = true;
                 break;
             }
-
-            if (!bHit) { newPosition += movement; }
-
-            // Handle gravity
-            allHits = Physics.SphereCastAll(newPosition + transform.rotation * animationHandler.LimbReferences.bottomPointOfCapsuleOffset,
-                                            animationHandler.LimbReferences.characterRadius, Physics.gravity, Physics.gravity.magnitude, Physics.AllLayers, QueryTriggerInteraction.Ignore);
-            System.Array.Sort(allHits, (x, y) => x.distance.CompareTo(y.distance));
-
-            bHit = false;
-            foreach (RaycastHit hit in allHits)
-            {
-                if (hit.distance == 0) { continue; }
-                if (hit.transform.root == transform) { continue; }
-                newPosition += Time.deltaTime * Mathf.Clamp01(hit.distance) * Physics.gravity;
-                bHit = true;
-                break;
-            }
-
-            if (!bHit) { newPosition += Physics.gravity * Time.deltaTime; }
-
-            transform.position = newPosition;
+            if (!bHit) { gravity += Physics.gravity * Time.fixedDeltaTime; }
+            isGrounded = bHit;
+            targetMovementPredictionRigidbodyPosition += gravity;
+            targetMovementPredictionRigidbodyPosition += movement;
+            Vector3 deltaPos = targetMovementPredictionRigidbodyPosition - movementPredictionRigidbody.position;
+            movementPredictionRigidbody.velocity = 1f / Time.fixedDeltaTime * deltaPos * Mathf.Pow(positionStrength, 90f * Time.fixedDeltaTime);
         }
 
         private NetworkVariable<Vector3> currentPosition = new NetworkVariable<Vector3>();
         private NetworkVariable<Quaternion> currentRotation = new NetworkVariable<Quaternion>();
         private void Update()
         {
+            UpdateLocomotion();
+            /*
             if (!IsSpawned) { return; }
 
             if (IsServer)
@@ -131,7 +177,7 @@ namespace Vi.ArtificialIntelligence
 
                 animationHandler.Animator.SetFloat("MoveForward", Mathf.MoveTowards(animationHandler.Animator.GetFloat("MoveForward"), animDir.z > 0.9f ? Mathf.RoundToInt(animDir.z) : animDir.z, Time.deltaTime * 5));
                 animationHandler.Animator.SetFloat("MoveSides", Mathf.MoveTowards(animationHandler.Animator.GetFloat("MoveSides"), animDir.x > 0.9f ? Mathf.RoundToInt(animDir.x) : animDir.x, Time.deltaTime * 5));
-            }
+            }*/
 
             /*
             weaponHandler.SetIsBlocking(isBlocking);
@@ -170,6 +216,23 @@ namespace Vi.ArtificialIntelligence
             {
                 transform.rotation = attributes.GetAilmentRotation();
             }*/
+        }
+
+        private void UpdateLocomotion()
+        {
+            float runSpeed = 5;
+
+            Vector3 movement = Time.deltaTime * (NetworkManager.NetworkTickSystem.TickRate / 2) * (targetMovementPredictionRigidbodyPosition - transform.position);
+            transform.position += movement;
+
+            animationHandler.Animator.speed = (Mathf.Max(0, 5 - attributes.GetMovementSpeedDecreaseAmount()) + attributes.GetMovementSpeedIncreaseAmount()) / runSpeed;
+
+            //if (attributes.ShouldApplyAilmentRotation())
+            //    transform.rotation = attributes.GetAilmentRotation();
+            //else if (weaponHandler.IsAiming())
+            //    transform.rotation = Quaternion.Slerp(transform.rotation, currentRotation.Value, Time.deltaTime * NetworkManager.NetworkTickSystem.TickRate);
+            //else
+            //    transform.rotation = Quaternion.Slerp(transform.rotation, currentRotation.Value, Time.deltaTime * NetworkManager.NetworkTickSystem.TickRate);
         }
 
         private void OnDrawGizmos()
--- Assets/Production Scripts/Core/Networking/NetworkStart.cs
+++ Assets/Production Scripts/Core/Networking/NetworkStart.cs
@@ -19,7 +19,7 @@ namespace Vi.Networking
             //if (SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null)
             //    NetworkManager.Singleton.StartServer();
             if (Application.isEditor)
-                NetworkManager.Singleton.StartServer();
+                NetworkManager.Singleton.StartHost();
             else
                 NetworkManager.Singleton.StartClient();
         }
