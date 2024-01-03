1ca994981cb37f6f8ffe00fb33c89ad3cf1ef696
MJGridlockGames <jomarlive@gmail.com>
2024-01-03 12:52:57 +0800

    Texture object added

---
+++ Assets/ExternalTextureLoader/ETLTextureObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ETLTextureObject : MonoBehaviour
+{
+  [SerializeField] Renderer objectTarget;
+  [SerializeField] int materialID;
+  public string textureName;
+  [Header("External Links")]
+
+  public bool imageClickable;
+  public string webLink;
+
+  public void ChangeImageFile(Texture2D netImage)
+  {
+    var forUpdating = objectTarget.materials;
+    forUpdating[materialID].SetTexture(textureName, netImage);
+    objectTarget.materials = forUpdating;
+  }
+
+  public void LoadImageFromWeb(string url)
+  {
+    StartCoroutine(ExternalFileLoaderWeb.DoImageWebRequest(url, ChangeImageFile));
+  }
+  public void Start()
+  {
+
+  }
+}
