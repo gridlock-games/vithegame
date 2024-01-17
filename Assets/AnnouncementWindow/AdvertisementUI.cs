faff3020f6d3a004b85e6ad8c2d9345828bbdcb5
MJGridlockGames <jomarlive@gmail.com>
2024-01-17 22:41:18 +0800

    Adlist

--- Assets/AnnouncementWindow/AdvertisementUI.cs
+++ Assets/AnnouncementWindow/AdvertisementUI.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AdvertisementUI : MonoBehaviour
 {
   [SerializeField] private List<AdvertiseItem> advertiseItems;
   int currentItem;
   public AdvertisementWindow window;
+  private List<Image> itemsList = new List<Image>();
 
+  [SerializeField] GameObject selectableItemPrefab;
+  [SerializeField] GameObject selectableItemList;
+  private int previousSelectedImage = 0;
+  [SerializeField] private Sprite activeImage;
+  [SerializeField] private Sprite inactiveImage;
 
   private void Awake()
   {
     window = this.gameObject.GetComponent<AdvertisementWindow>();
+    setupItemList();
     StartCoroutine(AdvertisementCoroutine());
   }
   // Start is called before the first frame update
@@ -44,14 +52,34 @@ public class AdvertisementUI : MonoBehaviour
     window.updateEvent(advertiseItems[currentItem]);
   }
 
+
+  void updateSetListButtons()
+  {
+    //change image from previous item
+    itemsList[previousSelectedImage].sprite = inactiveImage;
+    previousSelectedImage = currentItem;
+    itemsList[currentItem].sprite = activeImage;
+  }
+
+  void setupItemList()
+  {
+    for (int i = 0; i < advertiseItems.Count; i++)
+    {
+      var newItem = Instantiate(selectableItemPrefab, selectableItemList.transform);
+      itemsList.Add(newItem.GetComponent<Image>());
+    }
+  }
+
   IEnumerator AdvertisementCoroutine()
   {
-
-        while (true)
+    
+    while (true)
         {
+      updateSetListButtons();
       updateImage();
       yield return new WaitForSeconds(5);
       nextItem();
     }
     }
+
 }
