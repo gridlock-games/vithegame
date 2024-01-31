03f49314dc2fefadefceb8a382cd55331cb5496d
Patrick Seeman <patseem189@gmail.com>
2024-01-31 18:51:42 -0500

    fix hub button interactability

--- Assets/Production Scripts/UI/HubServerBrowser.cs
+++ Assets/Production Scripts/UI/HubServerBrowser.cs
@@ -13,15 +13,14 @@ namespace Vi.UI
         [SerializeField] private ServerListElement serverListElement;
         [SerializeField] private Transform serverListElementParent;
         [SerializeField] private Button closeServersMenuButton;
-
-        public Button connectButton;
-        public Button refreshServersButton;
+        [SerializeField] private Button connectButton;
+        [SerializeField] private Button refreshServersButton;
 
         List<ServerListElement> serverListElementList = new List<ServerListElement>();
         private void Update()
         {
             var networkTransport = NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>();
-            connectButton.interactable = serverListElementList.Exists(item => item.Server.ip == networkTransport.ConnectionData.Address & ushort.Parse(item.Server.port) == networkTransport.ConnectionData.Port) & !NetworkManager.Singleton.IsListening;
+            connectButton.interactable = serverListElementList.Exists(item => item.Server.ip == networkTransport.ConnectionData.Address & ushort.Parse(item.Server.port) == networkTransport.ConnectionData.Port);
 
             if (!WebRequestManager.Singleton.IsRefreshingServers)
             {
