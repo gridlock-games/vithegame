7e80471b8e3891503cafdf7aac3b05717cc55a72
MJGridlockGames <jomarlive@gmail.com>
2024-02-06 16:03:36 +0800

    Update download to add advance checking system and such.

--- Assets/UpdateKit/CDNService/CDNDownloadAndCheck.cs
+++ Assets/UpdateKit/CDNService/CDNDownloadAndCheck.cs
@@ -8,17 +8,43 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 public class CDNDownloadAndCheck : MonoBehaviour
 {
   [SerializeField]
-  private UnityEvent<float> ProgressEvent;
+  public UnityEvent<float> ProgressEvent;
 
   [SerializeField]
-  private UnityEvent<bool> CompletionEvent;
+  public UnityEvent<bool> CompletionEvent;
 
   [SerializeField]
-  private UnityEvent<bool> FailureEvent;
+  public UnityEvent<bool> FailureEvent;
 
   private bool completeDownloadSuccess = false;
 
-  public (bool result, long downloadSize) CheckDownloadSize(string key)
+  public (bool checkSuccessful,List<string> toDownload, float totalFileSize) CheckingForUpdate(List<string> key)
+  {
+    float totalBytes = 0;
+    bool successfulUpdateCheck = true;
+    List<string> downloadList = new List<string>();
+    for (int i = 0; i < key.Count; i++)
+    {
+      var result = CheckDownloadSize(key[i]);
+      if (result.downloadSize > 0 && result.checkingResult == true)
+      {
+        totalBytes += result.downloadSize;
+        downloadList.Add(key[i]);
+      }
+      else if (result.checkingResult == false)
+      {
+        //Send a Error that update checking has failed
+        FailureEvent.Invoke(false);
+        successfulUpdateCheck = false;
+        break;
+      }
+    }
+    //If there content on downlist as well totalBytes is > 0 then report that the game need updating.
+    // if successful Update check is false then the checks failed and required game restarts
+    return (successfulUpdateCheck, downloadList, totalBytes);
+    }
+
+  public (bool checkingResult, long downloadSize) CheckDownloadSize(string key)
   {
     // Check the download size
     AsyncOperationHandle<long> getDownloadSize = Addressables.GetDownloadSizeAsync(key);
@@ -98,5 +124,4 @@ public class CDNDownloadAndCheck : MonoBehaviour
     }
     Addressables.Release(downloadHandle); //Release the operation handle
   }
-}
 }
