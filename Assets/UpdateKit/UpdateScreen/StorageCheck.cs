1b4117209aaf288bf9e826a2212b2cb4dce98b49
MJGridlockGames <jomarlive@gmail.com>
2024-02-12 14:46:27 +0800

    Update StorageCheck.cs
    Added codes in relation to file check

--- Assets/UpdateKit/UpdateScreen/StorageCheck.cs
+++ Assets/UpdateKit/UpdateScreen/StorageCheck.cs
@@ -1,26 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class StorageCheck : MonoBehaviour
 {
-  [SerializeField] GameObject notificationWindowCanvas;
+  [SerializeField] private float sizeheadwaySpace = 1000.0f; //Mimicing 1Gb
+  [SerializeField] private GameObject notificationWindowCanvas;
   public string FileSizeError = $"This device storage space is not suffience to download additional content.";
-  UnityEvent refreshGame;
+  private UnityEvent refreshGame;
+
   // Start is called before the first frame update
-  void Start()
-    {
+  private void Start()
+  {
     refreshGame.AddListener(RestartSoftware);
-    }
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  private void Update()
+  {
+  }
+
+  public bool DeterminedFreeSizeOutput(float deviceFileSize)
+  {
+    float emptyStorageSize = GetDeviceFreeStorage();
+    if (emptyStorageSize + sizeheadwaySpace < deviceFileSize)
     {
-        
+      //Cancel the download and start notification
+      NotifyUser();
+      return false;
     }
+    else
+    {
+      return true;
+    }
+  }
 
-  void NotifyUser()
+  public float GetDeviceFreeStorage()
+  {
+#if UNITY_ANDROID
+    AndroidJavaObject statFs = new AndroidJavaObject("android.os.StatFs", Application.persistentDataPath);
+    long freeBytes = statFs.Call<long>("getAvailableBytes");
+    return (float)freeBytes;
+  }
+#endif
+#if UNITY_IOS
+    return 1000.0f;
+  }
+#endif
+  private void NotifyUser()
   {
     GameObject notificationWindow = notificationWindowCanvas.GetComponentInChildren<MessageNotificationObject>().gameObject;
     MessageNotificationObject messageNotification = notificationWindow.GetComponent<MessageNotificationObject>();
@@ -28,8 +54,7 @@ public class StorageCheck : MonoBehaviour
     Instantiate(notificationWindowCanvas);
   }
 
-  void RestartSoftware()
+  private void RestartSoftware()
   {
-
   }
 }
