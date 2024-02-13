f19b0502fab3f679cb54af1bc336ca2b411a5f76
Patrick Seeman <patseem189@gmail.com>
2024-02-13 16:15:09 -0500

    download progress bar in initialization screen

--- Assets/Production Scripts/Core/GameInitialization.cs
+++ Assets/Production Scripts/Core/GameInitialization.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.SceneManagement;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
 
 namespace Vi.Core
 {
     public class GameInitialization : MonoBehaviour
     {
         [SerializeField] private SceneReference baseSceneReference;
+        [SerializeField] private Text downloadProgressBarText;
+        [SerializeField] private Image downloadProgressBarImage;
 
         private void Start()
         {
@@ -18,14 +21,45 @@ namespace Vi.Core
 
         private IEnumerator LoadScenes()
         {
-            Debug.Log(baseSceneReference);
-
             AsyncOperationHandle<long> downloadSize = Addressables.GetDownloadSizeAsync(baseSceneReference);;
             yield return new WaitUntil(() => downloadSize.IsDone);
             Debug.Log(downloadSize.Result);
 
-            //yield return Addressables.LoadSceneAsync(baseSceneReference, LoadSceneMode.Additive);
-            //SceneManager.UnloadSceneAsync("Initialization", UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+            if (downloadSize.Result > 0)
+            {
+                AsyncOperationHandle downloadHandle = Addressables.DownloadDependenciesAsync(baseSceneReference);
+
+                float lastRateTime = -1;
+                float downloadRate = 0;
+                float lastBytesAmount = 0;
+                float totalMB = downloadHandle.GetDownloadStatus().TotalBytes * 0.000001f;
+                while (!downloadHandle.IsDone)
+                {
+                    downloadProgressBarImage.fillAmount = downloadHandle.GetDownloadStatus().Percent;
+
+                    float downloadedMB = downloadHandle.GetDownloadStatus().DownloadedBytes * 0.000001f;
+                    
+                    if (Time.time - lastRateTime >= 1)
+                    {
+                        downloadRate = downloadedMB - lastBytesAmount;
+                        lastBytesAmount = downloadedMB;
+                        lastRateTime = Time.time;
+                    }
+
+                    downloadProgressBarText.text = downloadedMB.ToString("F2") + " MB / " + totalMB.ToString("F2") + " MB" + " (" + downloadRate.ToString("F2") + "Mbps)";
+                    yield return null;
+                }
+
+                Addressables.Release(downloadHandle);
+            }
+
+            downloadProgressBarImage.fillAmount = 1;
+            downloadProgressBarText.text = "All Downloads Complete";
+
+            Addressables.Release(downloadSize);
+
+            yield return Addressables.LoadSceneAsync(baseSceneReference, LoadSceneMode.Additive);
+            SceneManager.UnloadSceneAsync("Initialization", UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
         }
     }
 }
