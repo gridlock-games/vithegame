a84854f625a743083d43a3e8036043531e2b5a66
MJGridlockGames <jomarlive@gmail.com>
2024-03-26 15:45:18 +0800

    Deeplinking setup 2 - added js file for implementation

---
+++ Assets/Deeplinker/JScode/deeplinker.js
@@ -0,0 +1,26 @@
+var endPath = window.location.search;
+var finalLink = "vigamegridlock://"
+const deviceOS = getOS();
+
+switch (deviceOS) {
+    case 'ios':
+        finalLink = "vigamegridlock://" + window.location.search;
+        break;
+    case 'Android':
+        finalLink = "vigamegridlock://" + window.location.search;
+    default:
+        break;
+}
+
+function getOS()
+{
+    const userAgent = window.navigator.userAgent;
+    if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+    {
+       return 'iOS';
+    }
+    else if( userAgent.match( /Android/i ) )
+    {
+       return 'Android';
+    }
+}
