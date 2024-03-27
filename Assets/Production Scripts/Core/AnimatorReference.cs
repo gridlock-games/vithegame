7759c89dacef06fc92d19981622f140da1e82925
Patrick Seeman <patseem189@gmail.com>
2024-03-27 00:27:33 -0400

    Update AnimatorReference.cs

--- Assets/Production Scripts/Core/AnimatorReference.cs
+++ Assets/Production Scripts/Core/AnimatorReference.cs
@@ -31,7 +31,6 @@ namespace Vi.Core
 
         public void ApplyCharacterMaterial(CharacterReference.CharacterMaterial characterMaterial)
         {
-            return;
             if (characterMaterial.materialApplicationLocation == CharacterReference.MaterialApplicationLocation.Body)
             {
                 CharacterReference.CharacterMaterial headMaterial = PlayerDataManager.Singleton.GetCharacterReference().GetCharacterMaterialOptions(characterMaterial.raceAndGender).Find(item => item.materialApplicationLocation == CharacterReference.MaterialApplicationLocation.Head & characterMaterial.material.name.Contains(string.Concat(item.material.name.Where(char.IsDigit))));
@@ -48,7 +47,6 @@ namespace Vi.Core
         private Dictionary<CharacterReference.EquipmentType, GameObject> wearableEquipmentInstances = new Dictionary<CharacterReference.EquipmentType, GameObject>();
         public void ApplyWearableEquipment(CharacterReference.WearableEquipmentOption wearableEquipmentOption, CharacterReference.RaceAndGender raceAndGender)
         {
-            return;
             if (wearableEquipmentInstances.ContainsKey(wearableEquipmentOption.equipmentType))
             {
                 if (wearableEquipmentInstances[wearableEquipmentOption.equipmentType])
