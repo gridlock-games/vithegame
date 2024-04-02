3bcbcb8cfe9d025d1f0fd77779a59501ddf3d77a
Patrick Seeman <patseem189@gmail.com>
2024-04-02 13:43:04 -0400

    target enemies by angle of look change

--- Assets/Production Scripts/Core/Player/PlayerMovementHandler.cs
+++ Assets/Production Scripts/Core/Player/PlayerMovementHandler.cs
@@ -374,7 +374,7 @@ namespace Vi.Player
                     CameraController cameraController = cameraInstance.GetComponent<CameraController>();
                     ExtDebug.DrawBoxCastBox(cameraController.CameraPositionClone.transform.position + weaponHandler.CurrentActionClip.boxCastOriginPositionOffset, weaponHandler.CurrentActionClip.boxCastHalfExtents, cameraController.CameraPositionClone.transform.forward, cameraController.CameraPositionClone.transform.rotation, weaponHandler.CurrentActionClip.boxCastDistance, Color.yellow);
                     RaycastHit[] allHits = Physics.BoxCastAll(cameraController.CameraPositionClone.transform.position + weaponHandler.CurrentActionClip.boxCastOriginPositionOffset, weaponHandler.CurrentActionClip.boxCastHalfExtents, cameraController.CameraPositionClone.transform.forward, cameraController.CameraPositionClone.transform.rotation, weaponHandler.CurrentActionClip.boxCastDistance, LayerMask.GetMask("NetworkPrediction"), QueryTriggerInteraction.Ignore);
-                    System.Array.Sort(allHits, (x, y) => x.distance.CompareTo(y.distance));
+                    List<KeyValuePair<NetworkCollider, float>> angleList = new List<KeyValuePair<NetworkCollider, float>>();
                     foreach (RaycastHit hit in allHits)
                     {
                         if (hit.transform.root.TryGetComponent(out NetworkCollider networkCollider))
@@ -382,12 +382,20 @@ namespace Vi.Player
                             if (PlayerDataManager.Singleton.CanHit(attributes, networkCollider.Attributes) & !networkCollider.Attributes.IsInvincible)
                             {
                                 Quaternion targetRot = Quaternion.LookRotation(networkCollider.transform.position - cameraController.CameraPositionClone.transform.position, Vector3.up);
-                                if (Mathf.Abs(targetRot.eulerAngles.y - cameraController.CameraPositionClone.transform.eulerAngles.y) < weaponHandler.CurrentActionClip.maximumTargetingRotationAngle)
-                                {
-                                    cameraController.AddRotation(0, Mathf.Clamp((targetRot.eulerAngles.y - cameraController.CameraPositionClone.transform.eulerAngles.y) * Time.deltaTime * LimbReferences.rotationConstraintOffsetSpeed, -LimbReferences.rotationConstraintOffsetSpeed, LimbReferences.rotationConstraintOffsetSpeed));
-                                }
-                                break;
+                                angleList.Add(new KeyValuePair<NetworkCollider, float>(networkCollider, targetRot.eulerAngles.y - cameraController.CameraPositionClone.transform.eulerAngles.y));
                             }
+                        }
+                    }
+
+                    angleList.Sort((x, y) => Mathf.Abs(x.Value).CompareTo(Mathf.Abs(y.Value)));
+                    foreach (var kvp in angleList)
+                    {
+                        Quaternion targetRot = Quaternion.LookRotation(kvp.Key.transform.position - cameraController.CameraPositionClone.transform.position, Vector3.up);
+                        if (Mathf.Abs(targetRot.eulerAngles.y - cameraController.CameraPositionClone.transform.eulerAngles.y) < weaponHandler.CurrentActionClip.maximumTargetingRotationAngle)
+                        {
+                            Debug.Log(Time.time + " Targeting - " + kvp.Key.Attributes.name);
+                            cameraController.AddRotation(0, Mathf.Clamp((targetRot.eulerAngles.y - cameraController.CameraPositionClone.transform.eulerAngles.y) * Time.deltaTime * LimbReferences.rotationConstraintOffsetSpeed, -LimbReferences.rotationConstraintOffsetSpeed, LimbReferences.rotationConstraintOffsetSpeed));
+                            break;
                         }
                     }
                 }
