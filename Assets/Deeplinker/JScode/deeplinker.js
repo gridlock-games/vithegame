8e42ded8453691d2b85d3d1d8371b3b18fd39321
MJGridlockGames <jomarlive@gmail.com>
2024-04-02 16:20:42 +0800

    Update deeplinker.js

--- Assets/Deeplinker/JScode/deeplinker.js
+++ Assets/Deeplinker/JScode/deeplinker.js
@@ -1,28 +1,42 @@
 var endPath = window.location.search;
 var finalLink = "vigamegridlock://"
 const deviceOS = getOS();
+var hasParameters = window.location.search.includes("code");
+console.log("valid content: " + hasParameters);
+//code validity
+if (hasParameters) {
+    switch (deviceOS) {
+        case 'ios':
+            finalLink = "vigamegridlock://authentication/" + window.location.search;
+            window.open(finalLink, "_blank");
+            break;
+        case 'Android':
+            finalLink = "vigamegridlock://authentication/" + window.location.search;
+            window.open(finalLink, "_blank");
+        default:
+            break;
 
-switch (deviceOS) {
-    case 'ios':
-        finalLink = "vigamegridlock://" + window.location.search;
-        window.open(finalLink, "_blank");
-        break;
-    case 'Android':
-        finalLink = "vigamegridlock://" + window.location.search;
-        window.open(finalLink, "_blank");
-    default:
-        break;
+    }
 }
 
-function getOS()
-{
+//Get device type
+function getOS() {
     const userAgent = window.navigator.userAgent;
-    if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
-    {
-       return 'iOS';
+    if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
+        return 'iOS';
     }
-    else if( userAgent.match( /Android/i ) )
-    {
-       return 'Android';
+    else if (userAgent.match(/Android/i)) {
+        return 'Android';
     }
 }
+//Web UI code
+window.onload = function () {
+    if (!hasParameters) {
+        document.getElementById("titleText").textContent = "Something has gone wrong";
+        document.getElementById("subtitleText").textContent = "[Insert Error Message Here]";
+    }
+    else {
+        document.getElementById("titleText").textContent = "Login Successful";
+        document.getElementById("subtitleText").textContent = "you should be redirected to the app";
+    }
+};
