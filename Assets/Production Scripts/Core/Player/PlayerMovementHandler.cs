beab54943ebaf9856caea7d904973a44dea83a54
Patrick Seeman <patseem189@gmail.com>
2024-04-12 13:24:06 -0400

    implement auto aim player pref

--- Assets/Production Scripts/Core/Player/PlayerMovementHandler.cs
+++ Assets/Production Scripts/Core/Player/PlayerMovementHandler.cs
@@ -388,7 +388,7 @@ namespace Vi.Player
             else
                 transform.rotation = Quaternion.Slerp(transform.rotation, movementPrediction.CurrentRotation, Time.deltaTime * NetworkManager.NetworkTickSystem.TickRate);
 
-            if (weaponHandler.CurrentActionClip.useRotationalTargetingSystem & cameraInstance)
+            if (weaponHandler.CurrentActionClip.useRotationalTargetingSystem & cameraInstance & bool.Parse(PlayerPrefs.GetString("AutoAim")))
             {
                 if (weaponHandler.IsInAnticipation | weaponHandler.IsAttacking)
                 {
