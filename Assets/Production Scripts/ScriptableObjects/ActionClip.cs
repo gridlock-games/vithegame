d9390022adb794175cd7aae4c21ed25e26831e06
Odale Patrick Roxas <roxasodale91@gmail.com>
2024-04-22 08:41:23 +0800

    action clip

--- Assets/Production Scripts/ScriptableObjects/ActionClip.cs
+++ Assets/Production Scripts/ScriptableObjects/ActionClip.cs
@@ -127,8 +127,8 @@ namespace Vi.ScriptableObjects
 
         public const float chargePenaltyTime = 1.0f;
         public const float enhanceChargeTime = 0.50f;
-        public const float chargeAttackTime = 0.10f;
-        public const float cancelChargeTime = 0.07f;
+        public const float chargeAttackTime = 0.08f;
+        public const float cancelChargeTime = 0.03f;
         public const float chargeAttackStateAnimatorTransitionDuration = 0.25f;
 
         public float attackingNormalizedTime = 0.25f;
