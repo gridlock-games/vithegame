008e44c8a1cadc00acb17bdabfbb0bec5a7ce596
Patrick Seeman <patseem189@gmail.com>
2024-04-24 16:57:50 -0400

    blank UI modification menu

---
+++ Assets/Production Scripts/UI/PauseMenu/UIModificationMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vi.Core;
+
+namespace Vi.UI
+{
+    public class UIModificationMenu : Menu
+    {
+        
+    }
+}
