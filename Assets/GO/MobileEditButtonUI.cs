e0aff104b538c539b6f08ef64595510ae147979f
MJGridlockGames <jomarlive@gmail.com>
2024-04-24 11:28:14 +0800

    Added reset method

--- Assets/GO/MobileEditButtonUI.cs
+++ Assets/GO/MobileEditButtonUI.cs
@@ -1,5 +1,4 @@
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,12 +10,17 @@ namespace Vi.UI
 
     //[SerializeField] public MoveUIDefinition[] convertedObject;
     [SerializeField] public List<MoveUIDefinition_Class> convertedObject;
-    [SerializeField] public UIDefinition[] platformUIDefinitions;
 
+    [SerializeField] public UIDefinition[] platformUIDefinitions;
+    [SerializeField] private PlatformUIDefinition pud;
+    [SerializeField] private MoveUIDefinition[] defaultSet;
+    [SerializeField] private GameObject pudUI;
 
     // Start is called before the first frame update
     private void Start()
     {
+      //Disable Platform UIDef after its run its course
+      pud.enabled = false;
     }
 
     // Update is called once per frame
@@ -38,7 +42,7 @@ namespace Vi.UI
         }
         //convertedObject[i].gameObjectToMove = editableUIObjects[i].gameObject.GetComponent<DragableUIObject>().moveUIDefIdentifier.actualGameObject; //Remove if broke/useless
         toConvertRaw.newAnchoredPosition = new float[] { editableUIObjects[i].anchoredPosition.x, editableUIObjects[i].anchoredPosition.y };
-        toConvertRaw.anchorMinOverride = new float[] { editableUIObjects[i].anchorMin.x, editableUIObjects[i].anchorMin.y }; 
+        toConvertRaw.anchorMinOverride = new float[] { editableUIObjects[i].anchorMin.x, editableUIObjects[i].anchorMin.y };
         toConvertRaw.shouldOverrideAnchors = false;
         toConvertRaw.anchorMaxOverride = new float[] { editableUIObjects[i].anchorMax.x, editableUIObjects[i].anchorMax.y };
         toConvertRaw.pivotOverride = new float[] { editableUIObjects[i].pivot.x, editableUIObjects[i].pivot.y };
@@ -50,7 +54,7 @@ namespace Vi.UI
       if (convertedObject != null)
       {
         //var convertedData = JsonUtility.ToJson(convertedObject);
-        
+
         var convertedData = JsonConvert.SerializeObject(convertedObject);
         Debug.Log(convertedData);
         PlayerPrefs.SetString("ButtonUiLayout", convertedData);
@@ -65,18 +69,50 @@ namespace Vi.UI
 
     public void ClosePrefab()
     {
-      Destroy(this.gameObject);
+      Destroy(pudUI);
     }
-    //private struct MoveUIDefinition
-    //{
-    //  public GameObject gameObjectToMove;
-    //  public Vector2 newAnchoredPosition;
-    //  public bool shouldOverrideAnchors;
-    //  public Vector2 anchorMinOverride;
-    //  public Vector2 anchorMaxOverride;
-    //  public Vector2 pivotOverride;
-    //}
+    public void ResetToDefault()
+    {
+      convertedObject = new List<MoveUIDefinition_Class>();
+      for (int i = 0; i < defaultSet.Length; i++)
+      {
+        MoveUIDefinition_Class toConvertRaw = new MoveUIDefinition_Class();
 
+        toConvertRaw.objectID = defaultSet[i].objectID;
+        if (toConvertRaw.objectID == null || toConvertRaw.objectID == "")
+        {
+          toConvertRaw.objectID = "unassign" + i;
+        }
+        //convertedObject[i].gameObjectToMove = editableUIObjects[i].gameObject.GetComponent<DragableUIObject>().moveUIDefIdentifier.actualGameObject; //Remove if broke/useless
+        toConvertRaw.newAnchoredPosition = new float[] { defaultSet[i].newAnchoredPosition.x, defaultSet[i].newAnchoredPosition.y };
+        toConvertRaw.anchorMinOverride = new float[] { defaultSet[i].anchorMinOverride.x, defaultSet[i].anchorMinOverride.y };
+        toConvertRaw.shouldOverrideAnchors = false;
+        toConvertRaw.anchorMaxOverride = new float[] { defaultSet[i].anchorMaxOverride.x, defaultSet[i].anchorMaxOverride.y };
+        toConvertRaw.pivotOverride = new float[] { defaultSet[i].pivotOverride.x, defaultSet[i].pivotOverride.y };
+        convertedObject.Add(toConvertRaw);
+      }
+
+      Debug.Log("Saving");
+      //Save the data to the user input
+      if (convertedObject != null)
+      {
+        //var convertedData = JsonUtility.ToJson(convertedObject);
+
+        var convertedData = JsonConvert.SerializeObject(convertedObject);
+        Debug.Log(convertedData);
+        PlayerPrefs.SetString("ButtonUiLayout", convertedData);
+        Debug.Log("Completed");
+      }
+
+      //private struct MoveUIDefinition
+      //{
+      //  public GameObject gameObjectToMove;
+      //  public Vector2 newAnchoredPosition;
+      //  public bool shouldOverrideAnchors;
+      //  public Vector2 anchorMinOverride;
+      //  public Vector2 anchorMaxOverride;
+      //  public Vector2 pivotOverride;
+      //}
+    }
   }
-
-}
+  }
