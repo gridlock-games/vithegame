739c0139d0bf0490e4911326d10115c818eb2bf0
Odale Roxas <roxasodale91@gmail.com>
2024-04-26 10:33:54 +0800

    Charge time adjustments

--- Assets/Production Scripts/ScriptableObjects/ActionClip.cs
+++ Assets/Production Scripts/ScriptableObjects/ActionClip.cs
@@ -125,9 +125,9 @@ namespace Vi.ScriptableObjects
         public float enhancedChargeDamageMultiplier = 1;
         public float chargePenaltyDamage = 10;
 
-        public const float chargePenaltyTime = 1.5f;
-        public const float enhanceChargeTime = 1.00f;
-        public const float chargeAttackTime = 0.50f;
+        public const float chargePenaltyTime = 1.0f;
+        public const float enhanceChargeTime = 0.50f;
+        public const float chargeAttackTime = 0.1f;
         public const float cancelChargeTime = 0.000f;
         public const float chargeAttackStateAnimatorTransitionDuration = 0.25f;
 
