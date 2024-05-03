7718ed4adb3c6e24355886ae349b5d7f7b9ec84d
Patrick Seeman <60397186+LightPat@users.noreply.github.com>
2024-05-02 21:35:44 -0400

    update team elimiation vi essence visuals

---
+++ Assets/Production Scripts/UI/GameModeManagerUI/TeamEliminationUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vi.UI
+{
+    public class TeamEliminationUI : MonoBehaviour
+    {
+        
+    }
+}
