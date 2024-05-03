d7ab0e6587d097619f545083bd8c0686c4061282
Patrick Seeman <60397186+LightPat@users.noreply.github.com>
2024-05-02 22:13:34 -0400

    team elimination vi essence in UI

--- Assets/Production Scripts/UI/GameModeManagerUI/TeamEliminationUI.cs
+++ Assets/Production Scripts/UI/GameModeManagerUI/TeamEliminationUI.cs
@@ -1,11 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Vi.Core.GameModeManagers;
 
 namespace Vi.UI
 {
-    public class TeamEliminationUI : MonoBehaviour
+    public class TeamEliminationUI : GameModeManagerUI
     {
-        
+        [SerializeField] private Image viLogoImage;
+        [SerializeField] private Sprite viEssenceIcon;
+
+        private const float colorTransitionSpeed = 8;
+
+        private TeamEliminationManager teamEliminationManager;
+        private Sprite originalViLogoSprite;
+        private new void Start()
+        {
+            base.Start();
+            teamEliminationManager = gameModeManager.GetComponent<TeamEliminationManager>();
+            originalViLogoSprite = viLogoImage.sprite;
+        }
+
+        private new void Update()
+        {
+            base.Update();
+
+            if (teamEliminationManager.IsViEssenceSpawned())
+            {
+                viLogoImage.sprite = viEssenceIcon;
+            }
+            else
+            {
+                viLogoImage.sprite = originalViLogoSprite;
+                //viEssenceImage.color = Color.Lerp(viEssenceImage.color, , Time.deltaTime * colorTransitionSpeed);
+            }
+        }
     }
 }
