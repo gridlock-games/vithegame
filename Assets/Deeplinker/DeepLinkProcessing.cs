4b0baa618a9d714b768380dd7ebcc75db2066778
MJGridlockGames <jomarlive@gmail.com>
2024-05-06 13:09:57 +0800

    Update DeepLinkProcessing.cs

--- Assets/Deeplinker/DeepLinkProcessing.cs
+++ Assets/Deeplinker/DeepLinkProcessing.cs
@@ -1,85 +1,128 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using Vi.UI.SimpleGoogleSignIn;
 
-public class DeepLinkProcessing : MonoBehaviour
-{
-  public static DeepLinkProcessing Instance { get; private set; }
-  public string deeplinkURL;
 
-  private void Awake()
+  public class DeepLinkProcessing : MonoBehaviour
   {
-    if (Instance == null)
+    public static DeepLinkProcessing Instance { get; private set; }
+    public string deeplinkURL;
+
+    public loginSiteSource lss;
+
+    public enum loginSiteSource
     {
-      Instance = this;
-      Application.deepLinkActivated += onDeepLinkActivated;
-      if (!string.IsNullOrEmpty(Application.absoluteURL))
+      inactive,
+      google,
+      apple,
+      facebook,
+      elonfailure,
+      steam,
+      epic,
+      origin
+    }
+
+    private void Awake()
+    {
+      if (Instance == null)
       {
-        // Cold start and Application.absoluteURL not null so process Deep Link.
-        onDeepLinkActivated(Application.absoluteURL);
+        Instance = this;
+        Application.deepLinkActivated += onDeepLinkActivated;
+        if (!string.IsNullOrEmpty(Application.absoluteURL))
+        {
+          // Cold start and Application.absoluteURL not null so process Deep Link.
+          onDeepLinkActivated(Application.absoluteURL);
+        }
+        // Initialize DeepLink Manager global variable.
+        else deeplinkURL = "[none]";
+        DontDestroyOnLoad(gameObject);
       }
-      // Initialize DeepLink Manager global variable.
-      else deeplinkURL = "[none]";
-      DontDestroyOnLoad(gameObject);
+      else
+      {
+        Destroy(gameObject);
+      }
     }
-    else
+
+    private void onDeepLinkActivated(string url)
     {
-      Destroy(gameObject);
+      deeplinkURL = url;
+      deepLinkTypeProcessing();
     }
-  }
 
-  private void onDeepLinkActivated(string url)
-  {
-    deeplinkURL = url;
-    deepLinkTypeProcessing();
-  }
-  private void deepLinkTypeProcessing()
-  {
-    string deeplinkID = deeplinkURL.Split('?')[0];
-    //Remove "vigamegridlock://"
-    deeplinkID = deeplinkID.Replace("vigamegridlock://", "");
-    deeplinkID = deeplinkID.Replace("/", "");
-    Debug.Log(deeplinkID);
-
-    //Add a check if user is login or not
-    switch (deeplinkID)
+    private void deepLinkTypeProcessing()
     {
-      case "authentication":
-        Debug.Log("Access Login System Deeplink");
-        loginVerification();
-        return;
-      case "profile":
-        Debug.Log("Access Profile Deeplink");
-        return;
-      case "gift":
-        Debug.Log("Access gift code system");
-        return;
-      default:
-        return;
-    }
-  }
-  private void loginVerification()
-  {
-    Debug.Log("LOGIN VERIFYABLE");
-    string loginData = deeplinkURL.Split('?')[1];
-    Debug.Log(loginData);
-    string fullLoginData = "?" + loginData;
-    GoogleAuth.deeplinkListener(fullLoginData);
+      string deeplinkID = deeplinkURL.Split('?')[0];
+      //Remove "vigamegridlock://"
+      deeplinkID = deeplinkID.Replace("vigamegridlock://", "");
+      deeplinkID = deeplinkID.Replace("/", "");
+      Debug.Log(deeplinkID);
 
-    //Sentitive item - Erase Deeplink after use
-    deeplinkURL = "[none]"; 
-  }
-  private void demotest()
-  {
-    // Pass the data to whatever it is needed
-    DemoDeepLinkPassthru ddlp = this.GetComponent<DemoDeepLinkPassthru>();
-    if (ddlp != null)
+      //Add a check if user is login or not
+      switch (deeplinkID)
+      {
+        case "authentication":
+          Debug.Log("Access Login System Deeplink");
+          loginVerification();
+          return;
+
+        case "profile":
+          Debug.Log("Access Profile Deeplink");
+          return;
+
+        case "gift":
+          Debug.Log("Access gift code system");
+          return;
+
+        default:
+          return;
+      }
+    }
+
+    private void loginVerification()
     {
-      Debug.Log("Detected File: " + deeplinkURL);
-      ddlp.updateOutputText(deeplinkURL);
+      Debug.Log("LOGIN VERIFYABLE");
+      string loginData = deeplinkURL.Split('?')[1];
+      Debug.Log(loginData);
+      string fullLoginData = "?" + loginData;
+
+      //Temp Setup
+      lss = loginSiteSource.google;
+      switch (lss)
+      {
+        case loginSiteSource.inactive:
+          //Do Nothing
+          break;
+        case loginSiteSource.google:
+          GoogleAuth.deeplinkListener(fullLoginData);
+          break;
+        case loginSiteSource.apple:
+          break;
+        case loginSiteSource.facebook:
+          break;
+        case loginSiteSource.elonfailure:
+          break;
+        case loginSiteSource.steam:
+          break;
+        case loginSiteSource.epic:
+          break;
+        case loginSiteSource.origin:
+          break;
+        default:
+          break;
+      }
+
+      //Sentitive item - Erase Deeplink after use
+      deeplinkURL = "[none]";
     }
 
+
+    private void demotest()
+    {
+      // Pass the data to whatever it is needed
+      DemoDeepLinkPassthru ddlp = this.GetComponent<DemoDeepLinkPassthru>();
+      if (ddlp != null)
+      {
+        Debug.Log("Detected File: " + deeplinkURL);
+        ddlp.updateOutputText(deeplinkURL);
+      }
+    }
   }
-}
