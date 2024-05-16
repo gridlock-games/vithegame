4620bb55c4c982900c5718d4b241cf9aef4ebd72
Patrick Seeman <patseem189@gmail.com>
2024-05-16 01:58:14 -0400

    black holeeeeeee prework

--- Assets/Production Scripts/Misc/PullRigidboiesToMe.cs
+++ Assets/Production Scripts/Misc/PullRigidboiesToMe.cs
@@ -1,11 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vi.Core;
 
 namespace Vi.Misc
 {
     public class PullRigidboiesToMe : MonoBehaviour
     {
-        
+        [SerializeField] private float radius = 2;
+        [SerializeField] private float forceMultiplier = 10;
+        [SerializeField] private GameObject[] VFXToPlayOnDestroy;
+
+        Collider[] colliders = new Collider[20];
+        private void FixedUpdate()
+        {
+            int count = Physics.OverlapSphereNonAlloc(transform.position, radius, colliders, LayerMask.GetMask(new string[] { "NetworkPrediction" }), QueryTriggerInteraction.Ignore);
+            for (int i = 0; i < count; i++)
+            {
+                if (colliders[i].TryGetComponent(out Rigidbody rb))
+                {
+                    float dist = Vector3.Distance(transform.position, rb.position);
+                    rb.AddForce((transform.position - rb.position).normalized * forceMultiplier, ForceMode.VelocityChange);
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            foreach (GameObject prefab in VFXToPlayOnDestroy)
+            {
+                PlayerDataManager.Singleton.StartCoroutine(WeaponHandler.DestroyVFXWhenFinishedPlaying(Instantiate(prefab, transform.position, transform.rotation)));
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, radius);
+        }
     }
 }
