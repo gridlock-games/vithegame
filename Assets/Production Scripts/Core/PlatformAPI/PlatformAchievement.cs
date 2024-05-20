d6e1d38df34e4cebd2189ac4a83b30789b58adba
MJGridlockGames <jomarlive@gmail.com>
2024-05-20 11:13:34 +0800

    Achievement End point

--- Assets/Production Scripts/Core/PlatformAPI/PlatformAchievement.cs
+++ Assets/Production Scripts/Core/PlatformAPI/PlatformAchievement.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Vi.PlatformAPI
+namespace PlatformAPI
 {
-  public class PlatformController : MonoBehaviour
+  public class PlatformAchievement : MonoBehaviour
   {
     // Start is called before the first frame update
     void Start()
@@ -17,13 +17,5 @@ namespace Vi.PlatformAPI
     {
 
     }
-
-    public void unlockAchievement(string achievementID)
-    {
-
-    }
-
-    public void changeRichPresence()
-    { }
   }
 }
