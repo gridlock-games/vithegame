d28da1db9d04d6fc941ced4b8fd8daf603359fbf
Patrick Seeman <60397186+LightPat@users.noreply.github.com>
2024-05-27 11:53:38 -0400

    improved char cards

--- Assets/Production Scripts/UI/CharacterSelect/CharacterCard.cs
+++ Assets/Production Scripts/UI/CharacterSelect/CharacterCard.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Vi.Core;
+using UnityEngine.EventSystems;
 
 namespace Vi.UI
 {
-    public class CharacterCard : MonoBehaviour
+    public class CharacterCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] private GameObject characterParent;
         [SerializeField] private GameObject addParent;
         [SerializeField] private GameObject lockedParent;
         [SerializeField] private Image selectedOverlay;
         [SerializeField] private Image selectedBorder;
+        [SerializeField] private Image selectedGlow;
         [SerializeField] private Text nameText;
         [SerializeField] private Text levelText;
 
@@ -73,17 +75,35 @@ namespace Vi.UI
         {
             selectedOverlay.fillAmount = isSelected ? 1 : 0;
             selectedBorder.fillAmount = isSelected ? 1 : 0;
+            selectedGlow.color = isSelected ? glowOnColor : glowOffColor;
             rt.sizeDelta = isSelected ? originalSizeDelta * selectedSizeMutliplier : originalSizeDelta;
         }
+
+        private readonly static Color glowOnColor = new Color(1, 1, 1, 1);
+        private readonly static Color glowOffColor = new Color(1, 1, 1, 0);
 
         private const float selectedSizeMutliplier = 1.13f;
         private const float selectedImageAnimationSpeed = 8;
         private bool isSelected;
         private void Update()
         {
-            selectedOverlay.fillAmount = Mathf.Lerp(selectedOverlay.fillAmount, isSelected ? 1 : 0, Time.deltaTime * selectedImageAnimationSpeed);
-            selectedBorder.fillAmount = Mathf.Lerp(selectedBorder.fillAmount, isSelected ? 1 : 0, Time.deltaTime * selectedImageAnimationSpeed);
-            rt.sizeDelta = Vector2.Lerp(rt.sizeDelta, isSelected ? originalSizeDelta * selectedSizeMutliplier : originalSizeDelta, Time.deltaTime * selectedImageAnimationSpeed);
+            selectedOverlay.fillAmount = Mathf.Lerp(selectedOverlay.fillAmount, isSelected | pointerIsHoveringOnThisObject ? 1 : 0, Time.deltaTime * selectedImageAnimationSpeed);
+            selectedBorder.fillAmount = Mathf.Lerp(selectedBorder.fillAmount, isSelected | pointerIsHoveringOnThisObject ? 1 : 0, Time.deltaTime * selectedImageAnimationSpeed);
+            selectedGlow.color = Color.Lerp(selectedGlow.color, isSelected | pointerIsHoveringOnThisObject ? glowOnColor : glowOffColor, Time.deltaTime * selectedImageAnimationSpeed);
+            rt.sizeDelta = Vector2.Lerp(rt.sizeDelta, isSelected | pointerIsHoveringOnThisObject ? originalSizeDelta * selectedSizeMutliplier : originalSizeDelta, Time.deltaTime * selectedImageAnimationSpeed);
+        }
+
+        private bool pointerIsHoveringOnThisObject;
+        public void OnPointerEnter(PointerEventData pointerEventData)
+        {
+            if (!IsCharacter) { return; }
+            pointerIsHoveringOnThisObject = true;
+        }
+
+        public void OnPointerExit(PointerEventData pointerEventData)
+        {
+            if (!IsCharacter) { return; }
+            pointerIsHoveringOnThisObject = false;
         }
     }
 }
