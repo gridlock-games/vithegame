3d6b252e3aadfae2dae5649892a2c76c5bfa20d1
Jomarcenter-mjm <jomarlive@gmail.com>
2024-05-29 16:03:54 +0800

    News UI

---
+++ Assets/NewsInfoSystem/NewUIScripts/NewsManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewsManager : MonoBehaviour
+{
+  [SerializeField] private UIDocument newsUIDocument;
+  [SerializeField] private string newsImageServerLocation = "https://vi-assets.com/imagestorage/News/";
+
+  // Start is called before the first frame update
+  void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
