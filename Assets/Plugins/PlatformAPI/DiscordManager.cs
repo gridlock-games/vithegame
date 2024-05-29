52bd70cbc07a1ac3c4a558cab8da8c3183bf465b
Patrick Seeman <patseem189@gmail.com>
2024-05-29 11:33:08 -0400

    fix missing bracket

--- Assets/Plugins/PlatformAPI/DiscordManager.cs
+++ Assets/Plugins/PlatformAPI/DiscordManager.cs
@@ -16,76 +16,73 @@ namespace jomarcentermjm.PlatformAPI
         void Start()
         {
 #if UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX
-            discord = new Discord.Discord(1181075969558183996, (System.UInt64)Discord.CreateFlags.NoRequireDiscord);
-            var activityManager = discord.GetActivityManager();
-            var activity = new Discord.Activity
-            {
-                State = "Login Menu",
-                Details = "Logging in to Vi",
-                Assets = {
+      discord = new Discord.Discord(1181075969558183996, (System.UInt64)Discord.CreateFlags.NoRequireDiscord);
+      var activityManager = discord.GetActivityManager();
+      var activity = new Discord.Activity
+      {
+        State = "Login Menu",
+        Details = "Logging in to Vi",
+        Assets = {
           LargeImage = "vi_game_logo",
           SmallImage = "vi_game_logo"
         }
-            };
-            activityManager.UpdateActivity(activity, (res) =>
-            {
-                if (res == Discord.Result.Ok)
-                {
-                    Debug.Log("Successful activity change");
-                }
-            });
+      };
+      activityManager.UpdateActivity(activity, (res) =>
+      {
+        if (res == Discord.Result.Ok)
+        {
+          Debug.Log("Successful activity change");
+        }
+      });
 #endif
         }
 
         public void ChangeActivityMessage(string state, string details, string largeimg, string largetext, string smallimage, string smalltext)
         {
 #if UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX
-            var activityManager = discord.GetActivityManager();
-            var activity = new Discord.Activity
-            {
-                State = "Login Menu",
-                Details = "Logging in to Vi",
-                Assets = {
+      var activityManager = discord.GetActivityManager();
+      var activity = new Discord.Activity
+      {
+        State = "Login Menu",
+        Details = "Logging in to Vi",
+        Assets = {
         LargeImage = largeimg,
         SmallImage = smallimage,
         SmallText = smalltext,
         LargeText = largetext
         }
-            };
-            activityManager.UpdateActivity(activity, (res) =>
-            {
-                if (res == Discord.Result.Ok)
-                {
-                    Debug.Log("Successful activity change");
-                }
-            });
+      };
+      activityManager.UpdateActivity(activity, (res) =>
+      {
+        if (res == Discord.Result.Ok)
+        {
+          Debug.Log("Successful activity change");
+        }
+      });
 #endif
         }
 
         public void clearActivity()
         {
 #if UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX
-            var activityManager = discord.GetActivityManager();
-            activityManager.ClearActivity((result) =>
-            {
-                if (result == Discord.Result.Ok)
-                {
-                    Debug.Log("End");
-                }
-                else
-                {
-                    Debug.Log("Failed");
-                }
-            });
-#endif
-        }
-
-#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX
-        // Update is called once per frame
-        void LateUpdate()
+      var activityManager = discord.GetActivityManager();
+      activityManager.ClearActivity((result) =>
+      {
+        if (result == Discord.Result.Ok)
         {
-            discord.RunCallbacks();
+          Debug.Log("End");
         }
+        else
+        {
+          Debug.Log("Failed");
+        }
+      });
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+      discord.RunCallbacks();
+    }
 #endif
     }
 }
