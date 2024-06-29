86e25677cc9e5e1df6ae345806332cdc05ec9dc7
Patrick Seeman <patseem189@gmail.com>
2024-06-29 15:41:55 -0400

    make lunges uninterruptable or invincible based on attack clip

--- Assets/Production Scripts/Core/AnimationHandler.cs
+++ Assets/Production Scripts/Core/AnimationHandler.cs
@@ -232,7 +232,11 @@ namespace Vi.Core
 
             if (actionClip.IsAttack() & actionClip.canLunge & !isFollowUpClip)
             {
-                ActionClip lungeClip = weaponHandler.GetWeapon().GetLungeClip();
+                ActionClip lungeClip = Instantiate(weaponHandler.GetWeapon().GetLungeClip());
+                lungeClip.name = lungeClip.name.Replace("(Clone)", "");
+                lungeClip.isInvincible = actionClip.isInvincible;
+                lungeClip.isUninterruptable = actionClip.isUninterruptable;
+
                 if (AreActionClipRequirementsMet(lungeClip))
                 {
                     // Lunge mechanic
