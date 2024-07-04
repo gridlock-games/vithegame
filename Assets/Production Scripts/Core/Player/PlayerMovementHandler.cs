3c123ecf0010f83d89d53c3cfe5633679c667828
Patrick Seeman <patseem189@gmail.com>
2024-07-04 11:27:10 -0400

    fix cursor lock state mode not resetting when abruptly disconnected from a server

--- Assets/Production Scripts/Core/Player/PlayerMovementHandler.cs
+++ Assets/Production Scripts/Core/Player/PlayerMovementHandler.cs
@@ -311,6 +311,7 @@ namespace Vi.Player
             if (IsLocalPlayer)
             {
                 UnityEngine.InputSystem.EnhancedTouch.EnhancedTouchSupport.Disable();
+                Cursor.lockState = CursorLockMode.None;
             }
         }
 
