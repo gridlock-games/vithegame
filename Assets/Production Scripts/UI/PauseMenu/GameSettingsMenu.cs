6c3b209eacb896be7786e166a3a16f7237e91020
Patrick Seeman <patseem189@gmail.com>
2024-07-07 20:16:25 -0400

    channel selector UI improvements

--- Assets/Production Scripts/UI/PauseMenu/GameSettingsMenu.cs
+++ Assets/Production Scripts/UI/PauseMenu/GameSettingsMenu.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Vi.Core;
 using UnityEngine.UI;
 using Vi.Utility;
+using TMPro;
+using Unity.Netcode;
 
 namespace Vi.UI
 {
@@ -14,6 +16,8 @@ namespace Vi.UI
         [SerializeField] private Toggle showFPSToggle;
         [SerializeField] private Toggle showPingToggle;
         [SerializeField] private Slider UIOpacitySlider;
+        [SerializeField] private GameObject channelDropdownParent;
+        [SerializeField] private TMP_Dropdown channelDropdown;
 
         private void SetPlayerPrefFromToggle(Toggle toggle, string playerPrefName)
         {
@@ -41,6 +45,39 @@ namespace Vi.UI
             UIOpacitySlider.minValue = Application.platform == RuntimePlatform.Android | Application.platform == RuntimePlatform.IPhonePlayer ? 0.1f : 0;
 
             UIOpacitySlider.onValueChanged.AddListener(delegate { SetPlayerPrefFromSlider(UIOpacitySlider, "UIOpacity"); });
+        }
+
+        private void Start()
+        {
+            if (PlayerDataManager.Singleton.ContainsId((int)NetworkManager.Singleton.LocalClientId)
+                & PlayerDataManager.Singleton.GetGameMode() == PlayerDataManager.GameMode.None
+                & !NetworkManager.Singleton.IsServer)
+            {
+                channelDropdownParent.SetActive(true);
+
+                channelDropdown.ClearOptions();
+                List<string> channelOptions = new List<string>();
+                List<int> channelCountList = PlayerDataManager.Singleton.GetChannelCountList();
+                for (int i = 0; i < channelCountList.Count; i++)
+                {
+                    channelOptions.Add("Channel " + (i + 1).ToString() + " - " + channelCountList[i].ToString() + " Players");
+                }
+                channelDropdown.AddOptions(channelOptions);
+                PlayerDataManager.PlayerData playerData = PlayerDataManager.Singleton.GetPlayerData((int)NetworkManager.Singleton.LocalClientId);
+                channelDropdown.value = playerData.channel;
+                channelDropdown.onValueChanged.AddListener(delegate { OnChannelDropdownChange(); });
+            }
+            else
+            {
+                channelDropdownParent.SetActive(false);
+            }
+        }
+
+        private void OnChannelDropdownChange()
+        {
+            var playerData = PlayerDataManager.Singleton.GetPlayerData((int)NetworkManager.Singleton.LocalClientId);
+            playerData.channel = channelDropdown.value;
+            PlayerDataManager.Singleton.SetPlayerData(playerData);
         }
     }
 }
