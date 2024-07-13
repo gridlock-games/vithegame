c76fbd8dfea7dc83e9cc7b5d1c09e0bd4d9b89f8
Patrick Seeman <patseem189@gmail.com>
2024-07-13 14:31:15 -0400

    optimize team name overrides and contains id logic

--- Assets/Production Scripts/Core/Singleton/PlayerDataManager.cs
+++ Assets/Production Scripts/Core/Singleton/PlayerDataManager.cs
@@ -230,6 +230,7 @@ namespace Vi.Core
         public bool TeamNameOverridesUpdatedThisFrame { get; private set; }
         private void OnTeamNameOverridesJsonChange(FixedString512Bytes prev, FixedString512Bytes current)
         {
+            if (!IsServer) { teamNameOverrides = JsonConvert.DeserializeObject<Dictionary<Team, TeamNameOverride>>(teamNameOverridesJson.Value.ToString()); }
             TeamNameOverridesUpdatedThisFrame = true;
             if (teamNameOverridesWasUpdatedThisFrameCoroutine != null) { StopCoroutine(teamNameOverridesWasUpdatedThisFrameCoroutine); }
             teamNameOverridesWasUpdatedThisFrameCoroutine = StartCoroutine(ResetTeamNameOverridesUpdatedBool());
@@ -244,7 +245,6 @@ namespace Vi.Core
 
         public string GetTeamText(Team team)
         {
-            Dictionary<Team, TeamNameOverride> teamNameOverrides = JsonConvert.DeserializeObject<Dictionary<Team, TeamNameOverride>>(teamNameOverridesJson.Value.ToString());
             if (teamNameOverrides != null)
             {
                 if (teamNameOverrides.ContainsKey(team)) { return teamNameOverrides[team].teamName; }
@@ -264,7 +264,6 @@ namespace Vi.Core
 
         public string GetTeamPrefix(Team team)
         {
-            Dictionary<Team, TeamNameOverride> teamNameOverrides = JsonConvert.DeserializeObject<Dictionary<Team, TeamNameOverride>>(teamNameOverridesJson.Value.ToString());
             if (teamNameOverrides != null)
             {
                 if (teamNameOverrides.ContainsKey(team))
@@ -280,7 +279,6 @@ namespace Vi.Core
 
         public string GetTeamPrefixRaw(Team team)
         {
-            Dictionary<Team, TeamNameOverride> teamNameOverrides = JsonConvert.DeserializeObject<Dictionary<Team, TeamNameOverride>>(teamNameOverridesJson.Value.ToString());
             if (teamNameOverrides != null)
             {
                 if (teamNameOverrides.ContainsKey(team)) { return teamNameOverrides[team].prefix; }
@@ -381,10 +379,7 @@ namespace Vi.Core
 
         public bool IdHasLocalPlayer(int clientId) { return localPlayers.ContainsKey(clientId); }
 
-        public bool ContainsId(int clientId)
-        {
-            return cachedPlayerDataList.Contains(new PlayerData(clientId));
-        }
+        public bool ContainsId(int clientId) { return cachedIdList.Contains(clientId); }
 
         public bool ContainsDisconnectedPlayerData(int clientId)
         {
@@ -875,8 +870,10 @@ namespace Vi.Core
         private void SyncCachedPlayerDataList()
         {
             cachedPlayerDataList.Clear();
+            cachedIdList.Clear();
             foreach (PlayerData playerData in playerDataList)
             {
+                cachedIdList.Add(playerData.id);
                 cachedPlayerDataList.Add(playerData);
             }
         }
@@ -1276,6 +1273,7 @@ namespace Vi.Core
 
         private NetworkList<PlayerData> playerDataList;
         private List<PlayerData> cachedPlayerDataList = new List<PlayerData>();
+        private List<int> cachedIdList = new List<int>();
 
         private NetworkList<DisconnectedPlayerData> disconnectedPlayerDataList;
 
