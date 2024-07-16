7ea25e51b981841b5e4c0a34840c54de92ba942c
Jomarcenter-mjm <jomarlive@gmail.com>
2024-07-16 21:58:49 +0800

    Update Steamworks

--- Assets/Plugins/PlatformAPI/SteamManager.cs
+++ Assets/Plugins/PlatformAPI/SteamManager.cs
@@ -115,7 +115,7 @@ namespace jomarcentermjm.PlatformAPI
 				{
 					Debug.Log("[Steamworks.NET] Shutting down because RestartAppIfNecessary returned true. Steam will restart the application.");
 
-					Application.Quit();
+					//Application.Quit();
 					return;
 				}
 			}
@@ -123,7 +123,7 @@ namespace jomarcentermjm.PlatformAPI
 			{ // We catch this exception here, as it will be the first occurrence of it.
 				Debug.LogError("[Steamworks.NET] Could not load [lib]steam_api.dll/so/dylib. It's likely not in the correct location. Refer to the README for more details.\n" + e, this);
 
-				Application.Quit();
+				//Application.Quit();
 				return;
 			}
 
