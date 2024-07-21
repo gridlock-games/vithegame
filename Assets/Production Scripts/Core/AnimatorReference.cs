0acaad83589688fe2ec114f28987b3eae8ef4c27
Patrick Seeman <60397186+LightPat@users.noreply.github.com>
2024-07-21 19:46:48 -0400

    fix wearable equipment getting disabled for some reason

--- Assets/Production Scripts/Core/AnimatorReference.cs
+++ Assets/Production Scripts/Core/AnimatorReference.cs
@@ -184,6 +184,7 @@ namespace Vi.Core
                     kvp.Value.transform.SetParent(null, true);
                     SceneManager.MoveGameObjectToScene(kvp.Value.gameObject, SceneManager.GetSceneByName(ObjectPoolingManager.instantiationSceneName));
                     ObjectPoolingManager.ReturnObjectToPool(pooledObject);
+                    kvp.Value.enabled = true;
                 }
                 foreach (SkinnedMeshRenderer smr in kvp.Value.GetRenderList())
                 {
