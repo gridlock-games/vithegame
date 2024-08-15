c709ee542ce78d0fe39bf343630191e668c75dfb
Patrick Seeman <patseem189@gmail.com>
2024-08-15 12:43:54 -0400

    revert changes

--- Assets/Production Scripts/ArtificialIntelligence/CaveCrasherController.cs
+++ Assets/Production Scripts/ArtificialIntelligence/CaveCrasherController.cs
@@ -79,16 +79,15 @@ namespace Vi.ArtificialIntelligence
 
             if (targetAttributes)
             {
-                weaponHandler.Ability1(true);
-                //float dist = Vector3.Distance(Destination, transform.position);
-                //if (dist < lightAttackDistance)
-                //{
-                //    weaponHandler.LightAttack(true);
-                //}
-                //else if (dist < ability1Distance & dist > ability1DistanceMin)
-                //{
-                //    weaponHandler.Ability1(true);
-                //}
+                float dist = Vector3.Distance(Destination, transform.position);
+                if (dist < lightAttackDistance)
+                {
+                    weaponHandler.LightAttack(true);
+                }
+                else if (dist < ability1Distance & dist > ability1DistanceMin)
+                {
+                    weaponHandler.Ability1(true);
+                }
             }
         }
 
