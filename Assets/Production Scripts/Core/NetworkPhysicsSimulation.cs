5c1d01faf4fdf4da987edbb3216468f8a59f26e6
Patrick Seeman <patseem189@gmail.com>
2024-09-07 00:47:30 -0400

    code cleanup

--- Assets/Production Scripts/Core/NetworkPhysicsSimulation.cs
+++ Assets/Production Scripts/Core/NetworkPhysicsSimulation.cs
@@ -8,22 +8,6 @@ namespace Vi.Core
 {
     public class NetworkPhysicsSimulation : MonoBehaviour
     {
-        private struct RigidbodyData
-        {
-            public Vector3 position;
-            public Vector3 velocity;
-            public Quaternion rotation;
-            public Vector3 angularVelocity;
-
-            public RigidbodyData(Rigidbody rb)
-            {
-                position = rb.position;
-                velocity = rb.velocity;
-                rotation = rb.rotation;
-                angularVelocity = rb.angularVelocity;
-            }
-        }
-
         public static void SimulateCertainObjects(Rigidbody[] rigidbodiesToSimulate)
         {
             Rigidbody[] allRigidbodies = FindObjectsByType<Rigidbody>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
