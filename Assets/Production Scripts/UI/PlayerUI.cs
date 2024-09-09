d1a2d904e7f278d98434906a73a3db7f7f0c4ba3
Patrick Seeman <patseem189@gmail.com>
2024-09-08 20:23:51 -0400

    Update PlayerUI.cs

--- Assets/Production Scripts/UI/PlayerUI.cs
+++ Assets/Production Scripts/UI/PlayerUI.cs
@@ -136,7 +136,7 @@ namespace Vi.UI
             {
                 ScrollToBottomOfTextChat();
                 actionMapHandler.OnTextChatOpen();
-                textChatInputField.ActivateInputField();
+                if (Application.platform != RuntimePlatform.Android & Application.platform != RuntimePlatform.IPhonePlayer) { textChatInputField.ActivateInputField(); }
                 unreadMessageCount = 0;
                 textChatMessageNumberText.text = "";
             }
@@ -165,7 +165,7 @@ namespace Vi.UI
         {
             textChat.SendTextChat(PlayerDataManager.Singleton.LocalPlayerData.character.name.ToString(), PlayerDataManager.Singleton.LocalPlayerData.team, textChatInputField.text);
             textChatInputField.text = "";
-            textChatInputField.ActivateInputField();
+            if (Application.platform != RuntimePlatform.Android & Application.platform != RuntimePlatform.IPhonePlayer) { textChatInputField.ActivateInputField(); }
         }
 
         private InputAction switchWeaponAction;
