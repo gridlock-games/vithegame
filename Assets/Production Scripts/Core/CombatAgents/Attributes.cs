43f47169145dee6a74321341d14103795e2b5c51
Patrick Seeman <patseem189@gmail.com>
2024-09-20 23:39:41 -0400

    buff the great sword charge attack

--- Assets/Production Scripts/Core/CombatAgents/Attributes.cs
+++ Assets/Production Scripts/Core/CombatAgents/Attributes.cs
@@ -628,6 +628,7 @@ namespace Vi.Core.CombatAgents
 
             StartHitStop(attacker, isMeleeHit);
 
+            Debug.Log(HPDamage);
             if (hitReaction.GetHitReactionType() == ActionClip.HitReactionType.Blocking)
             {
                 RenderBlock(impactPosition, runtimeWeapon ? runtimeWeapon.GetWeaponMaterial() : Weapon.WeaponMaterial.Metal);
