7548a24f03133fda74b0db10506f4dda8aced376
Patrick Seeman <patseem189@gmail.com>
2024-09-23 17:33:07 -0400

    fix combat agent null reference exception on mobile spectator

--- Assets/Production Scripts/Core/MovementHandlers/PhysicsMovementHandler.cs
+++ Assets/Production Scripts/Core/MovementHandlers/PhysicsMovementHandler.cs
@@ -29,10 +29,12 @@ namespace Vi.Core.MovementHandlers
 
         public Rigidbody Rigidbody { get { return rb; } }
         private Rigidbody rb;
+        protected CombatAgent combatAgent;
         protected override void Awake()
         {
             base.Awake();
             rb = GetComponentInChildren<Rigidbody>();
+            combatAgent = GetComponent<CombatAgent>();
             PooledObject pooledObject = GetComponent<PooledObject>();
             pooledObject.OnSpawnFromPool += OnSpawnFromPool;
             pooledObject.OnReturnToPool += OnReturnToPool;
