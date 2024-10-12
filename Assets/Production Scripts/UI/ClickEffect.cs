f5d412ba81dc446971cad75a4faaece7e49d1215
Patrick Seeman <patseem189@gmail.com>
2024-10-12 18:57:32 -0400

    test vibration

--- Assets/Production Scripts/UI/ClickEffect.cs
+++ Assets/Production Scripts/UI/ClickEffect.cs
@@ -56,6 +56,8 @@ namespace Vi.UI
                     Vector3 pos = cam.ScreenToWorldPoint(startPos);
                     emitSettings.position = pos;
                     particle.Emit(emitSettings, 1);
+
+                    Handheld.Vibrate();
                 }
             }
         }
