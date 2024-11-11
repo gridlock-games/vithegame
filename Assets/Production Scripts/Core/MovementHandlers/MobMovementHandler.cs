99a807ea3e2ba065f33be4213185685f361738cd
Patrick Seeman <patseem189@gmail.com>
2024-11-10 21:14:12 -0500

    sync mobs and bots

--- Assets/Production Scripts/Core/MovementHandlers/MobMovementHandler.cs
+++ Assets/Production Scripts/Core/MovementHandlers/MobMovementHandler.cs
@@ -323,13 +323,17 @@ namespace Vi.Core.MovementHandlers
             }
 
             bool evaluateForce = true;
-            if (weaponHandler.CurrentActionClip.shouldIgnoreGravity)
+            if (weaponHandler.CurrentActionClip.shouldIgnoreGravity & combatAgent.AnimationHandler.IsActionClipPlaying(weaponHandler.CurrentActionClip))
             {
-                if (combatAgent.AnimationHandler.IsActionClipPlaying(weaponHandler.CurrentActionClip))
+                if (movement.y >= 0)
                 {
                     Rigidbody.AddForce(movement - Rigidbody.linearVelocity, ForceMode.VelocityChange);
-                    evaluateForce = false;
                 }
+                else
+                {
+                    Rigidbody.AddForce(movement - new Vector3(Rigidbody.linearVelocity.x, 0, Rigidbody.linearVelocity.z), ForceMode.VelocityChange);
+                }
+                evaluateForce = false;
             }
 
             if (evaluateForce)
