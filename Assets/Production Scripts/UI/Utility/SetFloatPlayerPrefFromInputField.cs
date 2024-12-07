0c337d7e9cf7e9ef311bcf1c17ba08a80f10b784
Patrick Seeman <patseem189@gmail.com>
2024-12-07 08:40:25 -0500

    more UI utility scripts

---
+++ Assets/Production Scripts/UI/Utility/SetFloatPlayerPrefFromInputField.cs
@@ -0,0 +1,52 @@
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+using Vi.Utility;
+
+namespace Vi.UI
+{
+    [RequireComponent(typeof(InputField))]
+    public class SetFloatPlayerPrefFromInputField : MonoBehaviour
+    {
+        [SerializeField] private string playerPrefName;
+
+        private void SetPlayerPref(string value)
+        {
+            value = Regex.Replace(value, @"[^0-9|.]", "");
+            inputField.SetTextWithoutNotify(value);
+
+            if (float.TryParse(value, out float result))
+            {
+                FasterPlayerPrefs.Singleton.SetFloat(playerPrefName, result);
+            }
+            else
+            {
+                Debug.LogWarning("Unable to parse float from string " + value);
+            }
+        }
+
+        private InputField inputField;
+        private void Awake()
+        {
+            inputField = GetComponent<InputField>();
+        }
+
+        private void OnEnable()
+        {
+            if (FasterPlayerPrefs.Singleton.HasFloat(playerPrefName))
+            {
+                inputField.SetTextWithoutNotify(FasterPlayerPrefs.Singleton.GetFloat(playerPrefName).ToString());
+            }
+            else
+            {
+                Debug.LogWarning("No player pref found " + playerPrefName);
+            }
+            inputField.onValueChanged.AddListener(SetPlayerPref);
+        }
+
+        private void OnDisable()
+        {
+            inputField.onValueChanged.RemoveListener(SetPlayerPref);
+        }
+    }
+}
