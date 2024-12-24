95f4d4529be7f70bddcadc0a5a970186b6c9c5d6
Patrick Seeman <patseem189@gmail.com>
2024-12-23 22:59:00 -0500

    new status UI finished

--- Assets/Production Scripts/UI/StatusIconLayoutGroup.cs
+++ Assets/Production Scripts/UI/StatusIconLayoutGroup.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Vi.Core;
 using Vi.Core.CombatAgents;
 using Vi.ScriptableObjects;
 
 namespace Vi.UI
 {
+    [RequireComponent(typeof(Canvas))]
     public class StatusIconLayoutGroup : MonoBehaviour
     {
         private StatusAgent statusAgent;
@@ -26,13 +28,30 @@ namespace Vi.UI
         [SerializeField] private StatusIcon statusImagePrefab;
         private List<StatusIcon> statusIcons = new List<StatusIcon>();
 
+        private GridLayoutGroup gridLayoutGroup;
+        private HorizontalLayoutGroup horizontalLayoutGroup;
         private void Awake()
         {
+            gridLayoutGroup = GetComponent<GridLayoutGroup>();
+
+            horizontalLayoutGroup = GetComponent<HorizontalLayoutGroup>();
+
+            if (!gridLayoutGroup & !horizontalLayoutGroup)
+            {
+                Debug.LogWarning("Status icon layout group has no grid layout group and no horizontal layout group");
+            }
+
+            if (horizontalLayoutGroup)
+            {
+
+            }
+
             foreach (ActionClip.Status status in System.Enum.GetValues(typeof(ActionClip.Status)))
             {
                 StatusIcon statusIcon = Instantiate(statusImagePrefab.gameObject, transform).GetComponent<StatusIcon>();
                 statusIcon.InitializeStatusIcon(status);
                 statusIcons.Add(statusIcon);
+                statusIcon.SetActive(false);
                 statusIcon.gameObject.layer = gameObject.layer;
             }
         }
@@ -65,12 +84,30 @@ namespace Vi.UI
             if (!CanUpdateUI()) { return; }
 
             List<ActionClip.Status> activeStatuses = statusAgent.GetActiveStatuses();
+
+            int centerSiblingIndex = statusIcons.Count / (gridLayoutGroup ? 6 : 2);
+            int siblingOffset = 0;
+            int siblingOffsetMultiplier = 1;
+
             foreach (StatusIcon statusIcon in statusIcons)
             {
                 if (activeStatuses.Contains(statusIcon.Status))
                 {
                     statusIcon.SetActive(true);
-                    statusIcon.transform.SetAsFirstSibling();
+
+                    int newSiblingIndex = centerSiblingIndex + (siblingOffset * siblingOffsetMultiplier);
+                    if (newSiblingIndex >= transform.childCount)
+                    {
+                        newSiblingIndex = 0;
+                    }
+                    else if (newSiblingIndex < 0)
+                    {
+                        newSiblingIndex = 0;
+                    }
+
+                    statusIcon.transform.SetSiblingIndex(newSiblingIndex);
+                    if (siblingOffsetMultiplier == 1) { siblingOffset++; }
+                    siblingOffsetMultiplier *= -1;
                 }
                 else
                 {
@@ -86,6 +123,14 @@ namespace Vi.UI
 
             return true;
         }
+
+#if UNITY_EDITOR
+        [ContextMenu("Debug Max Status Count")]
+        private void DebugMaxStatusCount()
+        {
+            Debug.Log(System.Enum.GetValues(typeof(ActionClip.Status)).Length);
+        }
+#endif
     }
 }
 
