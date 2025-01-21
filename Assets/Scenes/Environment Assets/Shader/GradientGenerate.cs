9b671e3af3e479285ba05f97a411abf184d8f074
GenieDesu <75808214+GenieDesu@users.noreply.github.com>
2025-01-22 07:11:59 +0800

    Optimization

---
+++ Assets/Scenes/Environment Assets/Shader/GradientGenerate.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+[ExecuteInEditMode]
+public class GradientGenerate : MonoBehaviour
+{
+    public Material rocksMaterial;
+    public bool realtimeGeneration;
+    public Gradient lutGradient;
+    public Vector2Int lutTextureSize;
+    public Texture2D lutTexture;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Update()
+    {
+        if (realtimeGeneration)
+            GenerateLutTexture();
+    }
+
+    private void GenerateLutTexture()
+    {
+        lutTexture = new Texture2D(lutTextureSize.x, lutTextureSize.y)
+        { wrapMode = TextureWrapMode.Clamp };
+
+        for (var x = 0; x < lutTextureSize.x; x++)
+        {
+            var color = lutGradient.Evaluate(x / (float)lutTextureSize.x);
+            for (var y = 0; y < lutTextureSize.y; y++)
+            {
+                lutTexture.SetPixel(x, y, color);
+            }
+        }
+             lutTexture.Apply();
+        rocksMaterial.SetTexture("GradientRamp", lutTexture);
+        
+    }
+}
